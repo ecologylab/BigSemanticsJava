2e0dd46a561204f39901fd5b9df9f41cedd55a7f
damaraju <damaraju@ecologylab.net>
2008-02-14 22:26:59 +0000

    New MetaMetadata class as a top level element in the Recursive MetaMetadataField model.
    This is what should be returned by the MetaMetadataRepository

---
+++ ecologylab/semantics/metametadata/MetaMetadata.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ecologylab.semantics.metametadata;
+
+/**
+ * @author damaraju
+ *
+ */
+public class MetaMetadata extends MetaMetadataField
+{
+	
+	@xml_attribute String 		name;
+
+	public MetaMetadata()
+	{
+		super();
+	}
+
+}
