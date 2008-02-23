dae8bfbc09d39aa7e2db63b8e1a7f8c20ac07723
andruid <andruid@ecologylab.net>
2008-02-23 00:15:16 +0000

    centralize TAGS

---
+++ ecologylab/semantics/library/TypeTagNames.java
@@ -0,0 +1,19 @@
+package ecologylab.semantics.library;
+
+public interface TypeTagNames
+{
+	public static final String	DOCUMENT_TAG= "document";
+	
+	public static final String	IMAGE_TAG	= "image";
+
+	public static final String	FLICKR_TAG	= "flickr";
+
+	public static final String	SEARCH_TAG	= "search";
+
+	public static final String	ICDL_TAG	= "icdl";
+
+	public static final String	DLMS_TAG	= "dlms";
+
+	public static final String	RSS_TAG		= "rss";
+	
+}
