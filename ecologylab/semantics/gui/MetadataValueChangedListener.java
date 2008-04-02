d3c7f6648427f3e3d31df04da312751739743227
damaraju <damaraju@ecologylab.net>
2008-04-02 19:48:42 +0000

    Adding Valuechangelistener

--- ecologylab/semantics/gui/MetadataValueChangedListener.java
+++ ecologylab/semantics/gui/MetadataValueChangedListener.java
@@ -4,13 +4,16 @@
 package ecologylab.semantics.gui;
 
 import ecologylab.model.MetadataField;
+import ecologylab.semantics.metadata.Metadata;
 import ecologylab.semantics.metadata.MetadataFieldAccessor;
 
 /**
  * @author bharat
  *
  */
-public interface MetadataEndEditListener
+public interface MetadataValueChangedListener
 {
 	public void endEditHandler(String iconID, MetadataFieldAccessor metadataFieldAccessor);
+	
+	public void fieldValueChanged(Metadata metadata);
 }
