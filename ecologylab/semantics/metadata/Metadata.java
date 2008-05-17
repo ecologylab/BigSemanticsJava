341302a37f9fdec1b65e2ed16c495b1d84f12381
andruid <andruid@ecologylab.net>
2008-05-17 00:22:33 +0000

    Start new class for scalar Metadata fields that need a composite TermVector & a ParticipantInterest object.

--- ecologylab/semantics/metadata/Metadata.java
+++ ecologylab/semantics/metadata/Metadata.java
@@ -30,7 +30,7 @@ import ecologylab.xml.types.element.ArrayListState;
  * @author sashikanth
  *
  */
-abstract public class Metadata extends ElementState
+abstract public class Metadata extends MetadataBase
 {
 	MetaMetadata 			metaMetadata;
 	
@@ -41,20 +41,7 @@ abstract public class Metadata extends ElementState
 	 */
 	@xml_nested	ArrayListState<Metadata>	mixins = new ArrayListState<Metadata>();
 	
-	//FIXME -- not public!
-	public TermVector 				compositeTermVector;
-	
 	final static int		INITIAL_SIZE		= 5;
-
-	/**
-	 * Represents interest in this field as a whole
-	 * (Example: author = Ben Shneiderman), in addition to interest
-	 * propagated into the termVector, which represents interest in
-	 * particular Terms. While the latter is the basis of the information
-	 * retrieval model (IR), this is what enables the model to function
-	 * in the context of the semantic web / digital libraries.
-	 */  
-	ParticipantInterest		participantInterest = new ParticipantInterest();
 
 	/**
 	 * Set to true if this cFMetadata object was restored from a saved collage.
