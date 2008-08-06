8afd0ea060602bb0f71455187deb9af855cead6a
jmole <jmole@ecologylab.net>
2008-08-06 20:29:57 +0000

    Changed CompositeTermVector to have a method that adds a multiple of a termvector

--- ecologylab/semantics/model/text/XCompositeTermVector.java
+++ ecologylab/semantics/model/text/XCompositeTermVector.java
@@ -7,78 +7,104 @@ import java.util.Set;
 import java.util.Observer;
 
 public class XCompositeTermVector extends Observable implements Observer, ITermVector {
-	
-	public XCompositeTermVector() { }
-	
-	private XTermVector compositeTermVector;	
-	private HashSet<ITermVector> termVectors;
-	
-	/**
-	 * Adds a Term Vector to this Composite Term Vectors collection.
-	 * @param tv The Term Vector you wish to add.
-	 */
-	public void add(ITermVector tv) {
-		if(termVectors.add(tv)) {
-			tv.addObserver(this);
-			compositeTermVector.add(tv);
-			setChanged();
-			notifyObservers();
-		}
-	}
-	
-	/**
-	 * Removes a Term Vector from this Composite Term Vectors collection.
-	 * @param tv The Term Vector you wish to remove.
-	 */
-	public void remove(ITermVector tv) {
-		if(termVectors.remove(tv)) {
-			tv.deleteObserver(this);
-			compositeTermVector.add(-1,tv);
-			setChanged();
-			notifyObservers();
-		}
-	}
-	
-	// TODO: look in to sending the old value in "arg", 
-	// and then subtracting it, then adding the new one
-	// instead of rebuilding the whole thing each time.
-	public void update(Observable o, Object arg) {
-		if ((String)arg == "delete") {
-			ITermVector tv = (ITermVector) o;
-			remove(tv);
-		}
-		rebuildCompositeTermVector();
-		setChanged();
-		notifyObservers();
-	}
-	
-	public void recycle() {
-		for (ITermVector tv : termVectors)
-			tv.deleteObserver(this);
-	}
-	
-	private synchronized void rebuildCompositeTermVector() {
-		compositeTermVector = new XTermVector(compositeTermVector.size());
-		for (ITermVector tv : termVectors) {
-			compositeTermVector.add(tv);
-		}
-	}
-	
-	public double dot(VectorType<XTerm> v) {
-		return compositeTermVector.dot(v);
-	}
-	
-	public Set<XTerm> elements() {
-		return compositeTermVector.elements();
-	}
-	public double get(XTerm term) {
-		return compositeTermVector.get(term);
-	}
-	public HashMap<XTerm, Double> map() {
-		return compositeTermVector.map();
-	}
-	public Set<Double> values() {
-		return compositeTermVector.values();
-	}	
-	
+
+  public XCompositeTermVector() { }
+
+  private XTermVector compositeTermVector;	
+  private HashMap<ITermVector, Double> termVectors;
+
+  /**
+   * Adds a Term Vector to this Composite Term Vectors collection, multiplying it by a scalar.
+   * @param tv The Term Vector you wish to add.
+   * @param multiplier The scalar multiple.
+   */
+  public void add(double multiplier, ITermVector tv) {
+    HashMap<ITermVector, Double> v;
+    v = termVectors;
+    synchronized(v) {
+      double oldMultiplier = 0;
+      if(v.containsKey(tv))
+        oldMultiplier = v.get(tv);
+      else 
+        tv.addObserver(this);
+      v.put(tv, oldMultiplier + multiplier);
+      compositeTermVector.add(multiplier, tv);
+      setChanged();
+      notifyObservers();
+    }
+  }
+  
+  /**
+   * Adds a Term Vector to this Composite Term Vector's collection
+   * @param tv The term vector you wish to add.
+   */
+  public void add(ITermVector tv) {
+    add(1,tv);
+  }
+  
+  
+
+  /**
+   * Removes a Term Vector from this Composite Term Vectors collection.
+   * @param tv The Term Vector you wish to remove.
+   */
+  public void remove(ITermVector tv) {
+    Double multiple = termVectors.remove(tv);
+    if(multiple != null) {
+      tv.deleteObserver(this);
+      compositeTermVector.add(-multiple,tv);
+      setChanged();
+      notifyObservers();
+    }
+  }
+
+  // TODO: look in to sending the old value in "arg", 
+  // and then subtracting it, then adding the new one
+  // instead of rebuilding the whole thing each time.
+  public void update(Observable o, Object arg) {
+    if ((String)arg == "delete") {
+      ITermVector tv = (ITermVector) o;
+      remove(tv);
+    }
+    rebuildCompositeTermVector();
+    setChanged();
+    notifyObservers();
+  }
+
+  public void recycle() {
+    HashMap<ITermVector, Double> v;
+    v = termVectors;
+    if (v != null)
+      for (ITermVector tv : v.keySet())
+        tv.deleteObserver(this);
+  }
+
+  private synchronized void rebuildCompositeTermVector() {
+    HashMap<ITermVector, Double> v;
+    XTermVector c = compositeTermVector;
+    c = new XTermVector(c.size());
+    v = termVectors;
+    for (ITermVector t : v.keySet()) {
+      c.add(v.get(t), t);
+    }
+    compositeTermVector = c;
+  }
+
+  public double dot(VectorType<XTerm> v) {
+    return compositeTermVector.dot(v);
+  }
+
+  public Set<XTerm> elements() {
+    return compositeTermVector.elements();
+  }
+  public double get(XTerm term) {
+    return compositeTermVector.get(term);
+  }
+  public HashMap<XTerm, Double> map() {
+    return compositeTermVector.map();
+  }
+  public Set<Double> values() {
+    return compositeTermVector.values();
+  }	
+
 }
