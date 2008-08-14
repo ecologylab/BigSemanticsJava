67cd4a0a90f8ce8d855bd952a6b3b4f98f93574e
jmole <jmole@ecologylab.net>
2008-08-14 17:43:44 +0000

    committing null term vector

---
+++ ecologylab/semantics/model/text/NullTermVector.java
@@ -0,0 +1,50 @@
+package ecologylab.semantics.model.text;
+
+import java.util.HashMap;
+import java.util.Observer;
+import java.util.Set;
+
+public class NullTermVector implements ITermVector {
+
+	public static NullTermVector ntv = new NullTermVector();
+	
+	public static NullTermVector singleton() {
+		return ntv;
+	}
+	
+	public void addObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double dot(VectorType<XTerm> v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Set<XTerm> elements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double get(XTerm term) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public HashMap<XTerm, Double> map() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<Double> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
