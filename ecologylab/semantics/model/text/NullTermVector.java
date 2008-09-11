b1ddcd07ca90da02621f1b9e52bcdeb1c4d71b92
jmole <jmole@ecologylab.net>
2008-09-11 23:50:37 +0000

    Merging new semantics stuff into trunk.

---
+++ ecologylab/semantics/model/text/NullTermVector.java
@@ -0,0 +1,62 @@
+package ecologylab.semantics.model.text;
+
+import java.util.Hashtable;
+import java.util.Observer;
+import java.util.Set;
+
+import ecologylab.generic.VectorType;
+
+public class NullTermVector extends VectorType<XTerm> {
+
+	public static NullTermVector ntv = new NullTermVector();
+	
+	public static NullTermVector singleton() {
+		return ntv;
+	}
+	
+	public void addObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double dot(VectorType<XTerm> v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Set<XTerm> elements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double get(XTerm term) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Hashtable<XTerm, Double> map() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<Double> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public double norm() {
+	  return 0;
+	}
+
+  @Override
+  public double idfDot(VectorType<XTerm> v)
+  {
+    return 0;
+  }
+
+}
