c243b4dda95a17a86da573f8c0ee6c6f3c10305b
amathur <amathur@ecologylab.net>
2008-11-30 21:39:46 +0000

    Modified it so now all the translations for generated classes are placed in GeneratedMetadataTranslationScope.

--- ecologylab/semantics/library/DefaultMetadataTranslationSpace.java
+++ ecologylab/semantics/library/DefaultMetadataTranslationSpace.java
@@ -8,13 +8,7 @@ import ecologylab.semantics.library.scalar.MetadataInteger;
 import ecologylab.semantics.library.scalar.MetadataParsedURL;
 import ecologylab.semantics.library.scalar.MetadataString;
 import ecologylab.semantics.library.scalar.MetadataStringBuilder;
-import ecologylab.semantics.library.scholarlyPublication.Author;
-import ecologylab.semantics.library.scholarlyPublication.Reference;
-import ecologylab.semantics.library.scholarlyPublication.AcmPortal;
-import ecologylab.semantics.library.scholarlyPublication.Source;
-import ecologylab.semantics.library.uva.Topic;
 import ecologylab.semantics.metadata.DebugMetadata;
-import ecologylab.semantics.metadata.Media;
 import ecologylab.xml.TranslationScope;
 
 /**
@@ -30,29 +24,13 @@ public class DefaultMetadataTranslationSpace extends Debug
 	
 	protected static final Class TRANSLATIONS[] = 
 	{
+		DebugMetadata.class,
 		MetadataString.class,
 		MetadataStringBuilder.class,
 		MetadataParsedURL.class,
 		MetadataInteger.class,
-		DebugMetadata.class,
-		DcDocument.class,
-		Document.class,
-		Media.class,
-		Flickr.class,
-		IcdlImage.class,
-		Image.class,
-		Rss.class,
-		Search.class,
-		Text.class,
-		Pdf.class,
-		Author.class,
-		Reference.class,
-		Source.class,
-		AcmPortal.class,
 		
-		Topic.class,
-		
-		
+				
 	};
 
 	public static TranslationScope get()
