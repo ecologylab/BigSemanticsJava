2bcbc33a4f464f7d494e59a232755dc43cc500a6
andruid <andruid@ecologylab.net>
2009-04-15 02:51:07 +0000

    Refine fireEditValue().

--- ecologylab/semantics/metadata/MetadataFieldAccessor.java
+++ ecologylab/semantics/metadata/MetadataFieldAccessor.java
@@ -3,13 +3,16 @@
  */
 package ecologylab.semantics.metadata;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
+import ecologylab.generic.ReflectionTools;
 import ecologylab.gui.text.ExtentChangedEvent;
 import ecologylab.gui.text.ExtentChangedListener;
 import ecologylab.semantics.gui.EditValueEvent;
 import ecologylab.semantics.gui.EditValueListener;
 import ecologylab.semantics.gui.EditValueNotifier;
+import ecologylab.semantics.library.scalar.MetadataScalarBase;
 import ecologylab.xml.ElementState;
 import ecologylab.xml.FieldAccessor;
 import ecologylab.xml.FieldToXMLOptimizations;
@@ -25,6 +28,8 @@ public class MetadataFieldAccessor<M extends Metadata> extends FieldAccessor imp
 	
 	final private boolean		isMixin;
 	
+	Method									hwSetMethod;
+
 	private ArrayList<EditValueListener> editValueListeners = new ArrayList<EditValueListener>();
 	
 	public MetadataFieldAccessor(FieldToXMLOptimizations f2XO)
@@ -32,15 +37,18 @@ public class MetadataFieldAccessor<M extends Metadata> extends FieldAccessor imp
 		super(f2XO);
 		if (field != null)
 		{
-			isMixin			= field.isAnnotationPresent(MetadataBase.semantics_mixin.class);
+			isMixin							= field.isAnnotationPresent(MetadataBase.semantics_mixin.class);
 
 			Class<?> thatClass	= field.getType();
 			isPseudoScalar	= thatClass.isAnnotationPresent(semantics_pseudo_scalar.class);
+			
+			//TODO -- for future expansion??? andruid 4/14/09
+//			hwSetMethod					= ReflectionTools.getMethod(thatClass, "hwSet", SET_METHOD_ARG);
 		}
 		else
 		{
-			isMixin			= false;
-			isPseudoScalar	= false;		
+			isMixin							= false;
+			isPseudoScalar			= false;		
 		}
 	}
 	
@@ -62,12 +70,18 @@ public class MetadataFieldAccessor<M extends Metadata> extends FieldAccessor imp
 //		this.hwSet(context, newValue);
 //	}
 		
+	/**
+	 * 
+	 */
+	//FIXME -- this seems like a bogus idea for a scalar. it only makes sense for a composite type
 	public boolean hwSet(MetadataBase context, String newValue)
 	{
 //		this.set(context, newValue);
-		return context.hwSet(this.getTagName(), newValue);
+		return this.hwSet(context, newValue);
+//		return context.hwSet(this.getTagName(), newValue);
 	}
 	
+	//FIXME -- this looks wrong. why are we doing a lookup when we already have this?
 	public void set(Metadata context, String newValue)
 	{
 		context.set(this.getTagName(), newValue);
@@ -90,8 +104,10 @@ public class MetadataFieldAccessor<M extends Metadata> extends FieldAccessor imp
 	
 	public void fireEditValue(MetadataBase metadata, String fieldValueString)
 	{
-		if(this.hwSet(metadata, fieldValueString))
+		if(this.set(metadata, fieldValueString))	// uses reflection to call a set method or access the field directly if there is not one.
 		{
+			metadata.rebuildCompositeTermVector();	// makes this as if an hwSet().
+			
 			//Call the listeners only after the field is properly set.
 			EditValueEvent event = new EditValueEvent(this, metadata);
 
