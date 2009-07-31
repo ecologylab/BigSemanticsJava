6c04f7cee54ea14b1a1a3b1d47b721cd43dd1db1
amathur <amathur@ecologylab.net>
2009-07-31 21:32:01 +0000

    Moved it into semantics

--- ecologylab/semantics/metadata/Media.java
+++ ecologylab/semantics/metadata/Media.java
@@ -1,69 +1,121 @@
-/**
- * 
- */
 package ecologylab.semantics.metadata;
 
-import ecologylab.semantics.library.scalar.MetadataString;
+/**
+ * This is a generated code. DO NOT edit or modify it.
+ * 
+ * @author MetadataCompiler
+ **/
+
+import ecologylab.semantics.library.scalar.*;
+import ecologylab.semantics.metadata.*;
+import java.util.*;
 import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.net.ParsedURL;
+import ecologylab.generic.HashMapArrayList;
 import ecologylab.xml.xml_inherit;
-import ecologylab.xml.ElementState.xml_leaf;
-import ecologylab.xml.ElementState.xml_nested;
+import ecologylab.xml.types.element.Mappable;
+import ecologylab.semantics.library.DefaultMetadataTranslationSpace;
+import ecologylab.semantics.library.scholarlyPublication.*;
+import ecologylab.semantics.library.uva.*;
+import ecologylab.xml.TranslationScope;
+import ecologylab.xml.ElementState.xml_tag;
 
 /**
- * @author andruid
- *
- */
+ * The Media Class
+ **/
+
 @xml_inherit
+@xml_tag("media")
 public class Media extends Metadata
 {
 
-//	@xml_leaf	String context;
-//	@xml_nested MetadataString	context = new MetadataString();
-	@xml_nested MetadataString	context;
 	/**
-	 * 
-	 */
+	 * Constructor
+	 **/
+
 	public Media()
 	{
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
 	/**
-	 * @param metaMetadata
-	 */
+	 * Constructor
+	 **/
+
 	public Media(MetaMetadata metaMetadata)
 	{
 		super(metaMetadata);
-		// TODO Auto-generated constructor stub
 	}
 
-	MetadataString context()
+	/**
+	
+**/
+
+	@xml_tag("context")
+	@xml_nested
+	private MetadataString	context;
+
+	/**
+	 * Lazy Evaluation for context
+	 **/
+
+	public MetadataString context()
 	{
 		MetadataString result = this.context;
-		if(result == null)
+		if (result == null)
 		{
-			result 			= new MetadataString();
-			this.context 	= result;
+			result = new MetadataString();
+			this.context = result;
 		}
 		return result;
 	}
-	
+
+	/**
+	 * Gets the value of the field context
+	 **/
+
 	public String getContext()
 	{
 		return context().getValue();
 	}
 
+	/**
+	 * Sets the value of the field context
+	 **/
+
 	public void setContext(String context)
 	{
-//		this.context = context;
 		this.context().setValue(context);
 	}
-	
+
+	/**
+	 * The heavy weight setter method for field context
+	 **/
+
 	public void hwSetContext(String context)
 	{
-//		this.context = context;
-		this.setContext(context);
+		this.context().setValue(context);
 		rebuildCompositeTermVector();
 	}
 
+	/**
+	 * Sets the context directly
+	 **/
+
+	public void setContextMetadata(MetadataString context)
+	{
+		this.context = context;
+	}
+
+	/**
+	 * Heavy Weight Direct setter method for context
+	 **/
+
+	public void hwSetContextMetadata(MetadataString context)
+	{
+		if (this.context != null && this.context.getValue() != null && hasTermVector())
+			termVector().remove(this.context.termVector());
+		this.context = context;
+		rebuildCompositeTermVector();
+	}
 }
