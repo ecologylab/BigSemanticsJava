ffd8e33b1d8c00222a0217d42586b8a8ccf62d00
andruid <andruid@ecologylab.net>
2009-09-25 23:05:57 +0000

    New class for nested metadata

---
+++ ecologylab/semantics/metadata/builtins/Entity.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package ecologylab.semantics.metadata.builtins;
+
+import ecologylab.semantics.metadata.Metadata;
+import ecologylab.semantics.metadata.scalar.MetadataParsedURL;
+import ecologylab.semantics.metadata.scalar.MetadataString;
+import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.xml.xml_inherit;
+import ecologylab.xml.types.element.Mappable;
+
+/**
+ * This class represents a linked nested object in a metadata reference graph.
+ * The object will be resolved for presentation to the user as a linked entity,
+ * such as in InContextMetadata.
+ * 
+ * @author andruid
+ */
+@xml_inherit
+public class Entity<M extends Metadata> extends Metadata
+implements Mappable
+{
+	@xml_nested MetadataString		gist;
+	@xml_nested MetadataParsedURL	location;
+	
+	M															entity;
+	
+	
+	/**
+	 * 
+	 */
+	public Entity()
+	{
+
+	}
+
+	/**
+	 * @param metaMetadata
+	 */
+	public Entity(MetaMetadata metaMetadata)
+	{
+		super(metaMetadata);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object key()
+	{
+		return location;
+	}
+
+}
