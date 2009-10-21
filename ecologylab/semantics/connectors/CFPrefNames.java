ee317d6ab23337d41dffb28617dc1687abbc10f6
damaraju <damaraju@ecologylab.net>
2009-10-21 04:57:11 +0000

    cf/ app/CfPrefs - added prefs use_local_cf and local_cf_root model/CfInfoCollector - used these prefs to load the right metametadata config/preferences/metaprefs.xml,prefs.zip - metapref to set these two prefs in the CF gui
    semantics/connectors
    CFPrefNames - one and only one string object for each the prefs

--- ecologylab/semantics/connectors/CFPrefNames.java
+++ ecologylab/semantics/connectors/CFPrefNames.java
@@ -18,46 +18,39 @@ import ecologylab.appframework.types.prefs.PrefBoolean;
 
 public interface CFPrefNames
 {
-	public static final String	SEED_SET				= "seed_set";
-
-	public static final String	CF_APPLICATION_NAME		= "combinFormation";
-	
-	public static final String	DISPLAY_STATUS			= "display_status";
-	
-	public static final String  ENABLE_INCONTEXT_SLIDER = "incontext_slider";
-	
-	public static final String	APPLICATION_ENVIRONMENT	= "application_environment";
-
-	public static final String	DASHBOARD_ENABLED_NAME	= "dashboard_enabled";
-	
-	public static final String	DASHBOARD_NAME			= "dashboard";
-	
-	public static final String	SEED_VISIBILITY_CLOSED 	= "closed";
+	public static final String	SEED_SET									= "seed_set";
+	public static final String	CF_APPLICATION_NAME				= "combinFormation";
+	public static final String	DISPLAY_STATUS						= "display_status";
+	public static final String 	ENABLE_INCONTEXT_SLIDER 	= "incontext_slider";
+	public static final String	APPLICATION_ENVIRONMENT		= "application_environment";
+	public static final String	DASHBOARD_ENABLED_NAME		= "dashboard_enabled";
+	public static final String	DASHBOARD_NAME						= "dashboard";
+	public static final String	SEED_VISIBILITY_CLOSED 		= "closed";
 
 	/**
 	 * The name of the user interface currently in use, and its path in /config/interface.
 	 */
-	public static final String 	DEFAULT_INTERFACE  		= "in_context_interface";
+	public static final String 	DEFAULT_INTERFACE  				= "in_context_interface";
 
 	public static final String  USERINTERFACE_PREF_NAME		= "userinterface";
 
-	public static String 		INFO_EXTRACTION_PARAM	= "info_extraction_method";
+	public static String 		INFO_EXTRACTION_PARAM					= "info_extraction_method";
 	public static int 			OLD_UNSTRUCTURED_EXTRACTION		= 0;
 	public static int 			EUNYEE_STRUCTURED_EXTRACTION	= 1;
 
 	/**
 	 * The reduced interface for non-generative cF.
 	 */
-	public static final String REDUCED = "reduced";
+	public static final String REDUCED 										= "reduced";
 
-	public static final String		CURATED			= "curated";
+	public static final String		CURATED									= "curated";
 	
+	public static final String 	IGNORE_PDF								= "ignore_pdf";
 	
-	public static final String 	IGNORE_PDF			= "ignore_pdf";
+	public static final String	DECAY_INTEREST  					= "decay_interest";
+	
+	public static final String	LOCAL_CF_ROOT_PREF_NAME		= "local_cf_root";
+	public static final String	USE_LOCAL_CF_PREF_NAME				= "use_local_cf";
 	
 	public static final String	STUDY_COMPOSITION_SAVE_LOCATION	= "study_composition_save_location";
-	
-	public static final String	DECAY_INTEREST  = "decay_interest";
-	
-	public static final String ONE_CONTAINER_DOWNLOAD_PER_SITE = "one_container_download_per_site";
 }
