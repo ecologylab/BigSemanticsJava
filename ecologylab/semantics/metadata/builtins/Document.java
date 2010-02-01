08117d5714bb07162aab9a33ff5f1fdcf21a8695
andruid <andruid@ecologylab.net>
2010-02-01 03:16:52 +0000

    organize imports

--- ecologylab/semantics/metadata/builtins/Document.java
+++ ecologylab/semantics/metadata/builtins/Document.java
@@ -5,24 +5,13 @@ package ecologylab.semantics.metadata.builtins;
  * Metadata hierarchy. It is hand-authored in order to provide specific functionalities
  **/
 
-import ecologylab.semantics.library.scalar.*;
-import ecologylab.semantics.metadata.*;
+import ecologylab.net.ParsedURL;
+import ecologylab.semantics.metadata.Metadata;
 import ecologylab.semantics.metadata.scalar.MetadataInteger;
 import ecologylab.semantics.metadata.scalar.MetadataParsedURL;
 import ecologylab.semantics.metadata.scalar.MetadataString;
-
-import java.util.*;
 import ecologylab.semantics.metametadata.MetaMetadata;
-import ecologylab.net.ParsedURL;
-import ecologylab.generic.HashMapArrayList;
 import ecologylab.xml.xml_inherit;
-import ecologylab.xml.types.element.Mappable;
-import ecologylab.semantics.library.DefaultMetadataTranslationSpace;
-import ecologylab.semantics.library.scholarlyPublication.*;
-import ecologylab.semantics.library.uva.*;
-import ecologylab.xml.TranslationScope;
-import ecologylab.xml.ElementState.xml_nested;
-import ecologylab.xml.ElementState.xml_tag;
 
 /**
  * The Document Class
