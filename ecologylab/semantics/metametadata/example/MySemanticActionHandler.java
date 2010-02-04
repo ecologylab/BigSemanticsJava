83704cba4405ad454cb0c3737f83420da5a36ac1
quyin <quyin@ecologylab.net>
2010-02-04 20:46:54 +0000

    first steps to clean up the meta-metadata codes.

--- ecologylab/semantics/metametadata/example/MySemanticActionHandler.java
+++ ecologylab/semantics/metametadata/example/MySemanticActionHandler.java
@@ -8,16 +8,25 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collection;
 
+import ecologylab.concurrent.DownloadMonitor;
 import ecologylab.documenttypes.DocumentParser;
+import ecologylab.documenttypes.MetaMetadataSearchParser;
 import ecologylab.generic.ReflectionTools;
 import ecologylab.net.ParsedURL;
 import ecologylab.semantics.actions.SemanticAction;
 import ecologylab.semantics.actions.SemanticActionHandler;
 import ecologylab.semantics.actions.SemanticActionsKeyWords;
+import ecologylab.semantics.metadata.Metadata;
 import ecologylab.semantics.metadata.builtins.Document;
 import ecologylab.semantics.metametadata.Argument;
+import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.semantics.seeding.SearchResult;
+import ecologylab.semantics.seeding.Seed;
+import ecologylab.semantics.seeding.SeedDistributor;
 import ecologylab.xml.XMLTools;
 import ecologylab.semantics.actions.SemanticActionNamedArguments;
+import ecologylab.semantics.actions.exceptions.SemanticActionExecutionException;
+import ecologylab.semantics.connectors.ContentElement;
 import ecologylab.semantics.generated.library.WeatherReport;
 
 /**
@@ -30,16 +39,104 @@ public class MySemanticActionHandler extends SemanticActionHandler<MyContainer, 
 	public void backOffFromSite(SemanticAction action,
 			DocumentParser<MyContainer, ?, ?> documentType, MyInfoCollector infoCollector)
 	{
-		// TODO Auto-generated method stub
-
+		Argument domainArgA = getNamedArgument(action, DOMAIN);
+		String domain = domainArgA.getValue();
+		infoCollector.reject(domain);
+		// // this should be done in infoCollector.reject()
+		// infoCollector.removeAllCandidateContainersFromSite(site);
 	}
 
 	@Override
 	public void createAndVisualizeImgSurrogate(SemanticAction action,
 			DocumentParser<MyContainer, ?, ?> docType, MyInfoCollector infoCollector)
 	{
-		// TODO Auto-generated method stub
+		/*
+		try
+		{
+			// get the container
+			MyContainer container = docType.getContainer();
 
+			// get arguments
+			Argument imagePURLArgument = getNamedArgument(action, IMAGE_PURL);
+			Argument captionArgument = getNamedArgument(action, CAPTION);
+			Argument hrefPURLArgument = getNamedArgument(action, HREF);
+			Argument widthArgument = getNamedArgument(action, WIDTH);
+			Argument heightArgument = getNamedArgument(action, HEIGHT);
+
+			// get image url
+			ParsedURL imagePURL = (ParsedURL) semanticActionReturnValueMap.get(imagePURLArgument
+					.getValue());
+
+			// get caption
+			String caption = null;
+			// if we have passed caption also as an argument, we need to find it.
+			if (captionArgument != null)
+				caption = (String) semanticActionReturnValueMap.get(captionArgument.getValue());
+
+			// get href
+			ParsedURL hrefPURL = null;
+			if (hrefPURLArgument != null)
+				hrefPURL = (ParsedURL) semanticActionReturnValueMap.get(hrefPURLArgument.getValue());
+
+			// get width
+			int width = 0;
+			if (widthArgument != null)
+				width = (Integer) semanticActionReturnValueMap.get(widthArgument.getValue());
+
+			// get height
+			int height = 0;
+			if (heightArgument != null)
+				height = (Integer) semanticActionReturnValueMap.get(heightArgument.getValue());
+
+			MetaMetadata metaMetadata = infoCollector.metaMetaDataRepository().getImageMM(imagePURL);
+			if (imagePURL != null)
+			{
+				// look out for error condition
+				MetaMetadataSearchParser mmSearchType = docType instanceof MetaMetadataSearchParser ? (MetaMetadataSearchParser) docType
+						: null;
+				ImageElement imageElement = GlobalCollections.getNewImgElement(container, imagePURL,
+						hrefPURL, width, height, caption, false, metaMetadata);
+				if (imageElement != null)
+				{
+					// set the flags if any
+					setFlagIfAny(action, imageElement);
+					
+					// put it in the return value map
+					semanticActionReturnValueMap.put(action.getReturnValue(), imageElement);
+
+					SeedDistributor<CfContainer> resultsDistributor = null;
+					SearchResult searchResult = null;
+					if (mmSearchType != null)
+					{
+						Seed seed = mmSearchType.getSeed();
+						resultsDistributor = seed.seedDistributer(infoCollector);
+						if (resultsDistributor != null) // Case 1 - Image Search
+						{
+							int resultNum = mmSearchType.getResultNum();
+							imageElement.setSearchResult(infoCollector, resultsDistributor,
+									((MetaMetadataSearchParser) documentType).getResultSoFar());
+							resultsDistributor.queueResult(imageElement);
+						}
+					}
+					if (resultsDistributor == null) // Case 2
+						container.addToCandidateLocalImages(imageElement);
+					if (mmSearchType != null)
+					{ // FIXME -- is this enough to do if a search fails????!
+						mmSearchType.incrementResultSoFar();
+					}
+				}
+			}
+			else
+				error("Can't createAndVisualizeImgSurrogate because null PURL: " + metaMetadata.getName()
+						+ " - " + container.purl());
+
+		}
+		catch (Exception e)
+		{
+			// e.printStackTrace();
+			throw new SemanticActionExecutionException(e, action, semanticActionReturnValueMap);
+		}
+		*/
 	}
 
 	@Override
@@ -50,6 +147,7 @@ public class MySemanticActionHandler extends SemanticActionHandler<MyContainer, 
 
 	}
 
+	// seems that this is no longer needed
 	@Override
 	public MyContainer createContainer(SemanticAction action,
 			DocumentParser<MyContainer, ?, ?> docType, MyInfoCollector infoCollector)
@@ -197,16 +295,43 @@ public class MySemanticActionHandler extends SemanticActionHandler<MyContainer, 
 			MyInfoCollector infoCollector) throws IllegalArgumentException, IllegalAccessException,
 			InvocationTargetException
 	{
-		// TODO Auto-generated method stub
-
+		try
+		{
+				String actionName = "set" + XMLTools.javaNameFromElementName(action.getReturnValue(), true);
+				String object = action.getObject();
+				
+				// invoke it via reflection
+				handleGeneralAction(action, object, actionName);
+		}
+		catch (Exception e)
+		{
+			//e.printStackTrace();
+			throw new SemanticActionExecutionException(e, action, semanticActionReturnValueMap);
+		}
 	}
 
 	@Override
 	public void setMetadata(SemanticAction action, DocumentParser docType,
 			MyInfoCollector infoCollector)
 	{
-		// TODO Auto-generated method stub
+		try
+		{
+			// get the argument
+			Argument argument = getNamedArgument(action, FIELD_VALUE);
+			Metadata metadata = (Metadata) semanticActionReturnValueMap.get(argument.getValue());
 
+			// get the object
+			ContentElement container = (ContentElement) semanticActionReturnValueMap.get(action.getObject());
+
+			// invoke the actual method
+			if(container!=null)
+				container.setMetadata(metadata);
+		}
+		catch (Exception e)
+		{
+			//e.printStackTrace();
+			throw new SemanticActionExecutionException(e, action, semanticActionReturnValueMap);
+		}
 	}
 
 	@Override
@@ -220,8 +345,21 @@ public class MySemanticActionHandler extends SemanticActionHandler<MyContainer, 
 	public void parseLater(SemanticAction action, DocumentParser documentType,
 			MyInfoCollector infoCollector)
 	{
-		// TODO Auto-generated method stub
-
+		/*
+		try
+		{
+			MyContainer candidateContainer = getOrCreateContainer(action, documentType, infoCollector);
+			
+			if (candidateContainer != null)
+			{
+				// // TODO: implement this method in MyInfoCollector 
+				// infoCollector.addCandidateContainer(candidateContainer);
+			}
+		}
+		catch (Exception e)
+		{
+			throw new SemanticActionExecutionException(e, action, semanticActionReturnValueMap);
+		}*/
 	}
 
 	public void parseNow(SemanticAction action, DocumentParser docType, MyInfoCollector infoCollector)
