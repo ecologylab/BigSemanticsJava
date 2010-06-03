29be4e787ee8b9ee9bdd997ceae8a4406e43193e
andruid <andruid@ecologylab.net>
2010-06-03 02:33:14 +0000

    Metadata semantics & simpl tng integration. Metadata scalars as real scalar types (@xml_leaf, not @xml_nested). Bind meta-metadata with MetadataFieldDescriptor during MetaMetadataRepository.load. Resolve issues with translation scopes. Fix bug in initialization of ClassDescriptor subclasses.

---
+++ ecologylab/semantics/metametadata/test/SemanticsTest.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package ecologylab.semantics.metametadata.test;
+
+import java.io.File;
+
+import ecologylab.appframework.ApplicationEnvironment;
+import ecologylab.io.Assets;
+import ecologylab.net.ParsedURL;
+import ecologylab.semantics.generated.library.GeneratedMetadataTranslationScope;
+import ecologylab.semantics.metadata.builtins.Image;
+import ecologylab.xml.XMLTranslationException;
+
+/**
+ * @author andruid
+ *
+ */
+public class SemanticsTest extends ApplicationEnvironment
+{
+
+	/**
+	 * @param applicationName
+	 * @throws XMLTranslationException
+	 */
+	public SemanticsTest() throws XMLTranslationException
+	{
+		super("Semantics Test");
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param st
+	 */
+	public void fixCodeBase(String newProjectName)
+	{
+		File oldCodeBaseFile	= codeBase().file();
+		println("old codeBase = " + oldCodeBaseFile);
+		File parentFile				= oldCodeBaseFile.getParentFile();
+		File cfCodeBaseFile		= new File(parentFile, newProjectName);
+		ParsedURL cfCodeBasePurl	= new ParsedURL (cfCodeBaseFile);
+		setCodeBase(cfCodeBasePurl);
+		println("new codeBase = " + cfCodeBasePurl);
+		
+		println("Old Assets Root = " + Assets.getAssetsRoot());
+		File newAssetsRoot		= new File(cfCodeBaseFile, "config");
+		Assets.setAssetsRoot(new ParsedURL (newAssetsRoot));
+		println("New Assets Root = " + Assets.getAssetsRoot());
+		println("Cache Root = " + Assets.getCacheRoot());
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		try
+		{
+			SemanticsTest st	= new SemanticsTest();
+			st.fixCodeBase("cf");
+//			GeneratedMetadataTranslationScope.get();
+			new TestInfoCollector();
+			
+			Image i	= new Image();
+			i.setCaption("a nice caption.");
+			i.setContext("A much, much longer context");
+			
+			i.translateToXML(System.out);
+		
+		}
+		catch (XMLTranslationException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+
+
+}
