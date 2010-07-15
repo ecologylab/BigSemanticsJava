00b4c6873bb06ea6bdaf8656b8c19b370d594a6d
andruid <andruid@ecologylab.net>
2010-07-15 22:16:04 +0000

    Account for dates.

---
+++ ecologylab/semantics/metadata/scalar/types/MetadataDateScalarType.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package ecologylab.semantics.metadata.scalar.types;
+
+import java.util.Date;
+
+import ecologylab.semantics.metadata.scalar.MetadataDate;
+import ecologylab.serialization.ScalarUnmarshallingContext;
+
+/**
+ * @author andruid
+ *
+ */
+public class MetadataDateScalarType extends MetadataScalarScalarType<MetadataDate, Date>
+{
+	public MetadataDateScalarType()
+	{
+		super(MetadataDate.class, Date.class);
+	}
+
+
+	@Override
+	public MetadataDate getInstance(String value, String[] formatStrings,
+			ScalarUnmarshallingContext scalarUnmarshallingContext)
+	{
+		return new MetadataDate(getValueInstance(value, formatStrings, scalarUnmarshallingContext));
+	}
+
+
+	@Override
+	public String getCSharptType()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDbType()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getObjectiveCType()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
