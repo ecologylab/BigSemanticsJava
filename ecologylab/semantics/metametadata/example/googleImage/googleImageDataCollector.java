00469bdb523c521603c557cb9ffc85990e9dadb2
hyunjun <hyunjun@ecologylab.net>
2010-07-16 18:11:09 +0000

    update printOutToFile()

--- ecologylab/semantics/metametadata/example/googleImage/googleImageDataCollector.java
+++ ecologylab/semantics/metametadata/example/googleImage/googleImageDataCollector.java
@@ -1,22 +1,36 @@
 package ecologylab.semantics.metametadata.example.googleImage;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+
 import org.junit.Test;
 
+import ecologylab.generic.Debug;
 import ecologylab.net.ParsedURL;
+import ecologylab.semantics.actions.SemanticAction;
 import ecologylab.semantics.metametadata.example.MyInfoCollector;
 import ecologylab.semantics.metametadata.example.generated.GeneratedMetadataTranslationScope;
 
 public class googleImageDataCollector
 {
-	//List<GoogleImage> metadataCollected = ArrayList<GoogleImage>(); 
+//	List<GoogleImage> metadataCollected = ArrayList<GoogleImage>(); 
 	
 	public static void main(String[] args) throws InterruptedException
-	{
-		//SemanticAction.register(googleImageSemanticAction.class);
+	{ 
+		
+	}
+	
+	@Test
+	public void testCollector() throws InterruptedException{
+		SemanticAction.register(googleImageSemanticAction.class);
 		
 		MyInfoCollector mic = new MyInfoCollector("repo", GeneratedMetadataTranslationScope.get());
 		
 		ParsedURL pUrl = ParsedURL.getAbsolute("http://www.google.com/images?q=texas+site%3Awww.nytimes.com");
+		
+		ParsedURL pUrl2 = ParsedURL.getAbsolute("http://www.google.com/images?q=texas");
 		
 		mic.getContainerDownloadIfNeeded(null, pUrl, null, false, false, false);
 		
@@ -25,19 +39,57 @@ public class googleImageDataCollector
 		mic.getDownloadMonitor().stop(); 
 		
 		//print out result to file 
+//		printOutToFile("googleImage.out", metadataCollected);
 		
+		//print out result to database
 		
+	}
+	
+	/**
+	 * print out to file 
+	 * @param filename
+	 * @param metadataCollected
+	 * @throws FileNotFoundException
+	 */
+	private void printOutToFile(String filename, List metadataCollected) throws FileNotFoundException{
+		PrintWriter pw = new PrintWriter(new FileOutputStream(filename));
+		StringBuffer sb = new StringBuffer("#format:title,size,srcUrl,refUrl\n"); 
 		
-		//print out result to database 
+		if(metadataCollected.size() > 0){
+			for (Object obj : metadataCollected)
+			{
+//				if(obj instanceof GoogleImage){
+//					sb.append(obj.getTitle() + " " + obj.getSize() + " " + obj.getSrcUrl() + " " + obj.getRefUrl() + "\n");
+//					
+//				}
+			}
+			
+		}else
+			Debug.println("no results retrieved");
+		
+		pw.write(sb.toString()); 
+		pw.flush(); 
+		pw.close(); 
+		
+	}
+	
+	/**
+	 * connect db -> stmt -> executeUpdate
+	 * db schema generated from GoogleImage.class automatically using <link>SqlTranslator</link>
+	 * ref. DBUtil.class   
+	 *  
+	 * @param metadataCollected 
+	 */
+	private void printoutToDB(List metadataCollected){
 		
 		
 	}
 	
+	@Deprecated
 	@Test
 	public void testPUrl(){
 		ParsedURL pUrl = ParsedURL.getAbsolute("http://www.google.com/images?q=texas+site%3Awww.nytimes.com");
-		System.out.println(pUrl);
-		
+		System.out.println(pUrl.getRelative("ecologylab"));
 		
 	}
 	
