231683c7932196b8e186adcd1587aedeee1055d7
hyunjun <hyunjun@ecologylab.net>
2010-07-27 22:44:11 +0000

    organize imports

--- ecologylab/semantics/metametadata/example/bingImage/generated/Search.java
+++ ecologylab/semantics/metametadata/example/bingImage/generated/Search.java
@@ -8,28 +8,11 @@ This is a generated code. DO NOT edit or modify it.
 
 
 
-import ecologylab.generic.HashMapArrayList;
-import ecologylab.net.ParsedURL;
-import ecologylab.semantics.generated.library.*;
-import ecologylab.semantics.generated.library.bing.*;
-import ecologylab.semantics.generated.library.flickr.*;
-import ecologylab.semantics.metadata.Metadata;
-import ecologylab.semantics.metadata.MetadataBuiltinsTranslationScope;
-import ecologylab.semantics.metadata.builtins.*;
-import ecologylab.semantics.metadata.builtins.DebugMetadata;
+import java.util.ArrayList;
+
 import ecologylab.semantics.metadata.builtins.Document;
-import ecologylab.semantics.metadata.builtins.Entity;
-import ecologylab.semantics.metadata.builtins.Image;
-import ecologylab.semantics.metadata.builtins.Media;
-import ecologylab.semantics.metadata.scalar.*;
 import ecologylab.semantics.metametadata.MetaMetadataCompositeField;
-import ecologylab.semantics.metametadata.MetaMetadataRepository;
-import ecologylab.serialization.ElementState.xml_tag;
-import ecologylab.serialization.Hint;
-import ecologylab.serialization.TranslationScope;
 import ecologylab.serialization.simpl_inherit;
-import ecologylab.serialization.types.element.Mappable;
-import java.util.*;
 
 @simpl_inherit
 
