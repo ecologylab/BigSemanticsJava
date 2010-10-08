299bf9809d0699a021514813200d301511628016
quyin <quyin@ecologylab.net>
2010-10-08 18:16:16 +0000

    MetadataScalarScalarType.init(): a flag metadataScalarTypesRegistered is added to prevent initializing metadata scalar types twice.

--- ecologylab/semantics/metadata/scalar/types/MetadataScalarScalarType.java
+++ ecologylab/semantics/metadata/scalar/types/MetadataScalarScalarType.java
@@ -24,29 +24,31 @@ import ecologylab.serialization.types.scalar.TypeRegistry;
  */
 public abstract class MetadataScalarScalarType<M, T> extends ReferenceType<M>
 {
-	ScalarType<T>		valueScalarType;
-	
-	Field						valueField;
-	
+	ScalarType<T>						valueScalarType;
+
+	Field										valueField;
+
+	private static boolean	metadataScalarTypesRegistered	= false;
+
 	/**
 	 * @param thatClass
 	 */
 	public MetadataScalarScalarType(Class<M> metadataScalarTypeClass, Class valueClass)
 	{
 		super(metadataScalarTypeClass);
-		this.valueScalarType					= TypeRegistry.getType(valueClass);
+		this.valueScalarType = TypeRegistry.getType(valueClass);
 		valueField();
 	}
-	
+
 	Field valueField()
 	{
-		Field result	= valueField;
+		Field result = valueField;
 		if (result == null)
 		{
 			Class typeClass = MetadataScalarBase.class;
 			try
 			{
-				result							= typeClass.getDeclaredField(MetadataScalarBase.VALUE_FIELD_NAME);
+				result = typeClass.getDeclaredField(MetadataScalarBase.VALUE_FIELD_NAME);
 				result.setAccessible(true);
 				valueField = result;
 			}
@@ -61,49 +63,50 @@ public abstract class MetadataScalarScalarType<M, T> extends ReferenceType<M>
 		}
 		return result;
 	}
-	
+
 	/**
-	 * Set the value field inside the MetadataScalarBase subtype object that largerMetadataContext refers to.
-	 * Instantiate that MetadataScalarBase subtype object if necessary, and set it there.
+	 * Set the value field inside the MetadataScalarBase subtype object that largerMetadataContext
+	 * refers to. Instantiate that MetadataScalarBase subtype object if necessary, and set it there.
 	 * 
 	 * Used for deserializing.
 	 */
 	@Override
-  public boolean setField(Object largerMetadataContext, Field field, String valueString, String[] format, ScalarUnmarshallingContext scalarUnmarshallingContext)
-  {
-      if (valueString == null)
-          return true;
+	public boolean setField(Object largerMetadataContext, Field field, String valueString,
+			String[] format, ScalarUnmarshallingContext scalarUnmarshallingContext)
+	{
+		if (valueString == null)
+			return true;
 
-      boolean result		= false;
-      T valueObject;
+		boolean result = false;
+		T valueObject;
 
-      try
-      {
-          valueObject = valueScalarType.getInstance(valueString, format, scalarUnmarshallingContext);
-          if (valueObject != null)
-          {
-          	M metadataScalarContext	= (M) field.get(largerMetadataContext);
-          	if (metadataScalarContext == null)
-          	{
-          		metadataScalarContext = (M) ReflectionTools.getInstance(field.getType());
-          		field.set(largerMetadataContext, metadataScalarContext);
-          	}
-          	valueField().set(metadataScalarContext, valueObject);
-          	result 		= true;
-          }
-      }
-      catch (Exception e)
-      {
-          setFieldError(field, valueString, e);
-      }
-      return result;
-  }
+		try
+		{
+			valueObject = valueScalarType.getInstance(valueString, format, scalarUnmarshallingContext);
+			if (valueObject != null)
+			{
+				M metadataScalarContext = (M) field.get(largerMetadataContext);
+				if (metadataScalarContext == null)
+				{
+					metadataScalarContext = (M) ReflectionTools.getInstance(field.getType());
+					field.set(largerMetadataContext, metadataScalarContext);
+				}
+				valueField().set(metadataScalarContext, valueObject);
+				result = true;
+			}
+		}
+		catch (Exception e)
+		{
+			setFieldError(field, valueString, e);
+		}
+		return result;
+	}
 
 	/**
 	 * Called during deserialization.
 	 * 
-	 * Uses the valueScalarType stored inside the scalar type to get an instance of type T.
-	 * Called inside the various subtypes of this.
+	 * Uses the valueScalarType stored inside the scalar type to get an instance of type T. Called
+	 * inside the various subtypes of this.
 	 * 
 	 * @param value
 	 * @param formatStrings
@@ -115,59 +118,63 @@ public abstract class MetadataScalarScalarType<M, T> extends ReferenceType<M>
 	{
 		return valueScalarType.getInstance(value, formatStrings, scalarUnmarshallingContext);
 	}
-	
-	public static final Class[] METADATA_SCALAR_TYPES	=
+
+	public static final Class[]	METADATA_SCALAR_TYPES	=
+																										{ MetadataStringScalarType.class,
+			MetadataStringBuilderScalarType.class, MetadataIntegerScalarType.class,
+			MetadataParsedURLScalarType.class, MetadataDateScalarType.class, };
+
+	public static synchronized void init()
 	{
-			MetadataStringScalarType.class, MetadataStringBuilderScalarType.class, MetadataIntegerScalarType.class,
-			MetadataParsedURLScalarType.class, MetadataDateScalarType.class,
-	};
-	
-	public static void init()
-	{
-		TypeRegistry.register(METADATA_SCALAR_TYPES);
+		if (!metadataScalarTypesRegistered)
+		{
+			TypeRegistry.register(METADATA_SCALAR_TYPES);
+			metadataScalarTypesRegistered = true;
+		}
 	}
-	
+
 	@Override
 	public ScalarType operativeScalarType()
 	{
 		return valueScalarType;
 	}
-	
+
 	@Override
 	public Field operativeField(Field externalField)
 	{
 		return valueField;
 	}
-	
+
 	@Override
-  public String toString(Field field, Object largerMetadataContext)
-  {
-      String result = "COULDNT CONVERT!";
-      try
-      {
-      	M metadataScalarContext	= (M) field.get(largerMetadataContext);
-      	if (metadataScalarContext != null)
-      	{
-	      	T instance = (T) valueField().get(metadataScalarContext);
-	      	if (instance == null)
-	      		result = DEFAULT_VALUE_STRING;
-	      	else
-	      		result = toString(instance);
-      	}
-      	else
-      		result = DEFAULT_VALUE_STRING;
-      }
-      catch (Exception e)
-      {
-      	e.printStackTrace();
-      }
-      return result;
-  }
+	public String toString(Field field, Object largerMetadataContext)
+	{
+		String result = "COULDNT CONVERT!";
+		try
+		{
+			M metadataScalarContext = (M) field.get(largerMetadataContext);
+			if (metadataScalarContext != null)
+			{
+				T instance = (T) valueField().get(metadataScalarContext);
+				if (instance == null)
+					result = DEFAULT_VALUE_STRING;
+				else
+					result = toString(instance);
+			}
+			else
+				result = DEFAULT_VALUE_STRING;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return result;
+	}
+
 	public String toString(T instance)
 	{
 		return instance.toString();
 	}
-	
+
 	public boolean isDefaultValue(String value)
 	{
 		return valueScalarType.isDefaultValue(value);
