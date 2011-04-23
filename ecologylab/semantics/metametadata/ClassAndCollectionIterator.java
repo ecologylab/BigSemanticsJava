e735214eaebb303762879ef4dcbb4ddd0efbca7e
andruid <andruid@ecologylab.net>
2011-04-23 01:42:11 +0000

    formatted.

--- ecologylab/semantics/metametadata/ClassAndCollectionIterator.java
+++ ecologylab/semantics/metametadata/ClassAndCollectionIterator.java
@@ -8,116 +8,125 @@ import ecologylab.semantics.metadata.MetadataBase;
 import ecologylab.semantics.metadata.MetadataFieldDescriptor;
 
 /**
- * Iterates through a Collection of things, and then through an Iterator
- * of such (nested) Collections of things.
- * Provides flat access to all members.
+ * Iterates through a Collection of things, and then through an Iterator of such (nested)
+ * Collections of things. Provides flat access to all members.
  * 
  * @author jmole, damaraju
- *
- * @param <I>   Class that we iterate over.
- * @param <O>   Class of objects that are applied in the context of what we iterate over.
- *          This typically starts as this, but shifts as we iterate through 
- *          the nested Collection of Iterators.
+ * 
+ * @param <I>
+ *          Class that we iterate over.
+ * @param <O>
+ *          Class of objects that are applied in the context of what we iterate over. This typically
+ *          starts as this, but shifts as we iterate through the nested Collection of Iterators.
  */
-public class ClassAndCollectionIterator
-implements Iterator<MetadataBase>
+public class ClassAndCollectionIterator implements Iterator<MetadataBase>
 {
-  private Iterator<MetaMetadataField> iterator;
-  private Iterator<MetadataBase> collectionIterator;
-  private MetaMetadataField root;
-  private MetadataBase currentObject;
-  
-  private MetaMetadataField currentMMField;
+	private Iterator<MetaMetadataField>	iterator;
 
-	private Metadata metadata;
+	private Iterator<MetadataBase>			collectionIterator;
 
-  /**
-   * 
-   * @param firstObject - The object whose elements need to be iterated over.
-   */
-  public ClassAndCollectionIterator(MetaMetadataField firstObject, Metadata metadata)
-  {
-    root = firstObject;
-    this.iterator  = firstObject.iterator();
-    this.metadata = metadata;
-  }
+	private MetaMetadataField						root;
 
-  /**
-   * @return The next field in the Object.<br>
-   * If the next object is a non-null collection, it iterates through the objects of that collection 
-   */
-  public MetadataBase next() 
-  {
-    try
-    {
-      if (collectionIterator != null)
-        return nextInCollection();
-      
-      if (iterator.hasNext())
-      {
-      	MetaMetadataField firstNext = iterator.next(); 
-      	currentMMField = firstNext;
-        if (firstNext instanceof MetaMetadataCollectionField)
-        {
-        	MetadataFieldDescriptor mfd = firstNext.getMetadataFieldDescriptor();
-            Collection<MetadataBase> c = mfd.getCollection(metadata);
-            if (c != null)
-        	{
-            	collectionIterator = c.iterator();
-            	return nextInCollection();
-        	}
-        }
-        MetadataFieldDescriptor mfd = firstNext.getMetadataFieldDescriptor();
-        if (mfd == null)
-        {
-        	firstNext.error("Can't find MetadataFieldDescriptor. This probably means that the MetaMetadata compiler was not run or encountered errors!");
-        	return null;
-        }
-        MetadataBase md = (MetadataBase) mfd.getField().get((MetadataBase)metadata);
-        currentObject = md;
-        return md;
-      }
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
-    } catch (IllegalAccessException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	private MetadataBase								currentObject;
+
+	private MetaMetadataField						currentMMField;
+
+	private Metadata										metadata;
+
+	/**
+	 * 
+	 * @param firstObject
+	 *          - The object whose elements need to be iterated over.
+	 */
+	public ClassAndCollectionIterator(MetaMetadataField firstObject, Metadata metadata)
+	{
+		root = firstObject;
+		this.iterator = firstObject.iterator();
+		this.metadata = metadata;
 	}
-    return null;
-  }
 
-  private MetadataBase nextInCollection() 
-  {
-    if (!collectionIterator.hasNext()) {
-      collectionIterator = null;
-      return next();
-    } 
-    MetadataBase next = collectionIterator.next();
-    currentObject = next;
-    return next;
-  }
+	/**
+	 * @return The next field in the Object.<br>
+	 *         If the next object is a non-null collection, it iterates through the objects of that
+	 *         collection
+	 */
+	public MetadataBase next()
+	{
+		try
+		{
+			if (collectionIterator != null)
+				return nextInCollection();
 
-  /**
-   * 
-   * @return
-   */
-  public MetadataBase currentObject()
-  {
-    return currentObject;
-  }
+			if (iterator.hasNext())
+			{
+				MetaMetadataField firstNext = iterator.next();
+				currentMMField = firstNext;
+				if (firstNext instanceof MetaMetadataCollectionField)
+				{
+					MetadataFieldDescriptor mfd = firstNext.getMetadataFieldDescriptor();
+					Collection<MetadataBase> c = mfd.getCollection(metadata);
+					if (c != null)
+					{
+						collectionIterator = c.iterator();
+						return nextInCollection();
+					}
+				}
+				MetadataFieldDescriptor mfd = firstNext.getMetadataFieldDescriptor();
+				if (mfd == null)
+				{
+					firstNext
+							.error("Can't find MetadataFieldDescriptor. This probably means that the MetaMetadata compiler was not run or encountered errors!");
+					return null;
+				}
+				MetadataBase md = (MetadataBase) mfd.getField().get((MetadataBase) metadata);
+				currentObject = md;
+				return md;
+			}
+		}
+		catch (IllegalArgumentException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-  public void remove() 
-  {
-    throw new UnsupportedOperationException();
-  }
+	private MetadataBase nextInCollection()
+	{
+		if (!collectionIterator.hasNext())
+		{
+			collectionIterator = null;
+			return next();
+		}
+		MetadataBase next = collectionIterator.next();
+		currentObject = next;
+		return next;
+	}
 
-  public boolean hasNext()
-  {    
-    return iterator.hasNext() || (collectionIterator != null && collectionIterator.hasNext());
-  }
-  
-  
-  public MetaMetadataField getCurrentMMField()
+	/**
+	 * 
+	 * @return
+	 */
+	public MetadataBase currentObject()
+	{
+		return currentObject;
+	}
+
+	public void remove()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean hasNext()
+	{
+		return iterator.hasNext() || (collectionIterator != null && collectionIterator.hasNext());
+	}
+
+	public MetaMetadataField getCurrentMMField()
 	{
 		return currentMMField;
 	}
