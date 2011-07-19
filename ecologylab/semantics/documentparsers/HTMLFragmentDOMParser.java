aa61a1999abd471ce5b0a9e9935bfe00de43b5c7
andruid <andruid@ecologylab.net>
2011-07-19 14:56:41 +0000

    Re-write to use getElementsByTagName() on DOM; don't do a DomWalker!

--- ecologylab/semantics/documentparsers/HTMLFragmentDOMParser.java
+++ ecologylab/semantics/documentparsers/HTMLFragmentDOMParser.java
@@ -4,11 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import ecologylab.generic.DomTools;
+import ecologylab.net.ParsedURL;
 import ecologylab.semantics.collecting.SemanticsSessionScope;
 import ecologylab.semantics.html.DOMFragmentInformationTagger;
 import ecologylab.semantics.html.DOMParserInterface;
+import ecologylab.semantics.html.DOMWalkInformationTagger;
 import ecologylab.semantics.html.ImgElement;
 import ecologylab.semantics.metadata.builtins.AnonymousDocument;
+import ecologylab.serialization.XMLTools;
 
 public class HTMLFragmentDOMParser
 extends HTMLDOMParser
@@ -16,7 +23,17 @@ implements DOMParserInterface
 {
 
 	InputStream fragmentStream;
-	public DOMFragmentInformationTagger taggedDoc;
+	
+	ArrayList<ImgElement> imageElements	= new ArrayList<ImgElement>();
+	
+	ParsedURL							containerPurl;
+	
+	public ParsedURL getContainerPurl()
+	{
+		return containerPurl;
+	}
+
+	StringBuilder					bodyTextBuffy	= new StringBuilder();
 	
 	public HTMLFragmentDOMParser(SemanticsSessionScope infoCollector, InputStream inputStream)
 	{
@@ -29,8 +46,40 @@ implements DOMParserInterface
 	@Override
 	public void parse() throws IOException
 	{
-		taggedDoc = new DOMFragmentInformationTagger(null, this);
-		taggedDoc.generateCollectionsFromRoot(this.getRootNode());
+		org.w3c.dom.Document doc	= getDom();
+		DomTools.prettyPrint(doc);
+		NodeList bodyNodeList	= doc.getElementsByTagName("body");
+		if (bodyNodeList.getLength() > 0)
+		{
+			Node bodyNode				= bodyNodeList.item(0);
+			
+			NodeList children = bodyNode.getChildNodes();
+			if (children.getLength() > 0)
+			{
+				Node container 			= children.item(0).getAttributes().getNamedItem("container");
+				if (container != null)
+				{
+					String containerValue = container.getNodeValue();
+					if (containerValue != null && containerValue.length() > 0)
+					{
+						containerValue= XMLTools.unescapeXML(containerValue);
+						containerPurl = ParsedURL.getAbsolute(containerValue);
+					}
+				}
+			}
+			DOMWalkInformationTagger.getTextInSubTree(bodyNode, true, bodyTextBuffy, true);
+		}
+		NodeList imgNodeList	= doc.getElementsByTagName("img");
+		int numImages = imgNodeList.getLength();
+		if (numImages > 0)
+		{
+			for (int i=0; i < numImages; i++)
+			{
+				Node imgNode = imgNodeList.item(i);
+				imageElements.add(new ImgElement(imgNode, containerPurl));
+			}
+		}
+//		taggedDoc.generateCollectionsFromRoot(rootNode);
 	}
 	
 	public InputStream inputStream()
@@ -40,12 +89,13 @@ implements DOMParserInterface
 	
 	public String getDNDText()
 	{
-		return taggedDoc.getDNDText();
+		return bodyTextBuffy.toString();
 	}
 	
 	public ArrayList<ImgElement> getDNDImages()
 	{
-		return taggedDoc.getDNDImages();
+		
+		return imageElements;
 	}
 
 	public void setContent()
