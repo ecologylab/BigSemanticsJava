117d53eaaf5cf9a1534ef2448021d6ea0e861afc
andruid <andruid@ecologylab.net>
2011-07-19 14:53:37 +0000

    move pretty printer out

--- ecologylab/semantics/documentparsers/ImageParserAwt.java
+++ ecologylab/semantics/documentparsers/ImageParserAwt.java
@@ -445,50 +445,6 @@ public class ImageParserAwt extends DocumentParser<Image>
 		System.out.println();
 		return tagList;
 	}
-  public static void printTree(Node doc) 
-  {
-  	printTree(doc, 0);
-  }
-  public static void printTree(Node node, int level) 
-  {
-    try 
-    {
-    	for (int i=0; i< level; i++)
-    		System.out.print('\t');
-    	
-      System.out.print("<" + node.getNodeName());
-      NamedNodeMap attrMap = node.getAttributes();
-      for (int i = 0; i < attrMap.getLength(); i++) 
-      {
-        Node attr = attrMap.item(i);
-        String attrName = attr.getNodeName();
-				System.out.print(" " + attrName + "=\"" + attr.getNodeValue() + '"');
-      }
-      String value	= node.getNodeValue();
-      if (value != null)
-      	System.out.print(value);
-      System.out.print(">");
-
-      NodeList nl = node.getChildNodes();
-      int numChildren = nl.getLength();
-      if (numChildren > 0)
-      {
-        System.out.print("\n");
-				for (int i = 0; i < numChildren; i++) 
-	      {
-	        Node childNode = nl.item(i);
-	        printTree(childNode, level + 1);
-	      }
-	    	for (int i=0; i< level; i++)
-	    		System.out.print('\t');
-      }
-      System.out.println("</" + node.getNodeName() + ">");
-    } catch (Throwable e) 
-    {
-      System.out.println("Cannot print!! " + e.getMessage());
-    }
-  }
-
 	
 	@Override
 	public synchronized void recycle()
