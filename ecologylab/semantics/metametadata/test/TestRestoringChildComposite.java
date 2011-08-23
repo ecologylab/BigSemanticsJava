3aa3225acea8327531efd8670bb12234ac11c62d
quyin <quyin@ecologylab.net>
2011-08-23 03:05:54 +0000

    MetaMetadataNestedField: added recursivelyRestoreChildComposite() to facilitate re-serialization of repository after inheritance. See ecologylabSemanticsExample:ecologylab.semantics.metametadata.test.TestRestoringChildComposite for the usage.

---
+++ ecologylab/semantics/metametadata/test/TestRestoringChildComposite.java
@@ -0,0 +1,39 @@
+package ecologylab.semantics.metametadata.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.Test;
+
+import ecologylab.semantics.collecting.SemanticsSessionScope;
+import ecologylab.semantics.cyberneko.CybernekoWrapper;
+import ecologylab.semantics.generated.library.RepositoryMetadataTranslationScope;
+import ecologylab.semantics.metametadata.MetaMetadata;
+import ecologylab.semantics.metametadata.MetaMetadataRepository;
+import ecologylab.serialization.SIMPLTranslationException;
+import ecologylab.serialization.TranslationScope;
+
+public class TestRestoringChildComposite
+{
+
+	private SemanticsSessionScope semanticsSessionScope;
+	
+	@Test
+	public void testRestoringChildComposite() throws SIMPLTranslationException, IOException
+	{
+		TranslationScope metadataTranslationScope = RepositoryMetadataTranslationScope.get();
+		semanticsSessionScope = new SemanticsSessionScope(metadataTranslationScope, CybernekoWrapper.class);
+		MetaMetadataRepository repository = semanticsSessionScope.getRepository(); // after inheritance and binding
+		for (MetaMetadata globalMmd : repository.values())
+			globalMmd.recursivelyRestoreChildComposite();
+		for (Map<String, MetaMetadata> packageMmdScope : repository.getPackageMmdScopes().values())
+		{
+			for (MetaMetadata packageMmd : packageMmdScope.values())
+				packageMmd.recursivelyRestoreChildComposite();
+		}
+		
+		repository.serialize(new File("data/test-restore-child-composite.xml"));
+	}
+	
+}
