0758066c1dbcbaec27f64c25435b59f054ef2e2a
quyin <quyin@ecologylab.net>
2011-08-30 03:44:45 +0000

    <search_result> extends <document>: heading -> title, link -> location, snippet -> description. all uses of <search_result> changed accordingly. generated semantics re-generated.

--- ecologylab/semantics/metametadata/metasearch/SearchDispatcher.java
+++ ecologylab/semantics/metametadata/metasearch/SearchDispatcher.java
@@ -98,7 +98,7 @@ public class SearchDispatcher extends Debug implements Continuation<DocumentClos
 						if (i < numSearchResults)
 						{
 							SearchResult result = search.getSearchResults().get(i);
-							if (result.getHeading() != null)
+							if (result.getTitle() != null)
 							{
 								result.setEngine(search.getLocation().domain());
 								renderer.appendMetadata(result);
