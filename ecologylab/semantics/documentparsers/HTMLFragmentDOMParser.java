781370b0d413054f3c285f06d7cb04fd8ac01470
rhema <rhema@ecologylab.net>
2011-10-15 17:37:42 +0000

    Removed comment text from appearing on drag which fixes windows problem of beginframent endfragment.

--- ecologylab/semantics/documentparsers/HTMLFragmentDOMParser.java
+++ ecologylab/semantics/documentparsers/HTMLFragmentDOMParser.java
@@ -24,11 +24,11 @@ public class HTMLFragmentDOMParser extends HTMLDOMParser implements DOMParserInt
 
 	public static final String	HTML_TAG_IMG	= "img";
 
-	InputStream						fragmentStream;
+	InputStream									fragmentStream;
 
-	ArrayList<ImgElement>	imageElements	= new ArrayList<ImgElement>();
+	ArrayList<ImgElement>				imageElements	= new ArrayList<ImgElement>();
 
-	ParsedURL							containerPurl;
+	ParsedURL										containerPurl;
 
 	public ParsedURL getContainerPurl()
 	{
@@ -45,184 +45,142 @@ public class HTMLFragmentDOMParser extends HTMLDOMParser implements DOMParserInt
 		this.documentClosure = anonymousDocument.getOrConstructClosure();
 	}
 
-	
-	
 	/*
-	This is the old parse method as of oct 13, 2011.
-	This will be removed as soon as I am happy tweaking the old one.
-	-Rhema
-	
-	@Override
-	public void parse() throws IOException
-	{
-		org.w3c.dom.Document doc = getDom();
-		int containerNodeIndex = 0;
-		NodeList bodyNodeList = doc.getElementsByTagName(HTML_TAG_BODY);
-		//types not passed well... check here
-		if (bodyNodeList.getLength() > 0)
-		{
-			Node bodyNode = bodyNodeList.item(0);
-
-			NodeList children = bodyNode.getChildNodes();
-			if (children.getLength() > 0)
-			{
-				for (int i = 0; i < children.getLength(); i++)
-				{
-					if (children.item(i).getNodeType() == Node.ELEMENT_NODE)
-					{
-						containerNodeIndex = i;
-						break;
-					}
-				}
-				Node containerNode;
-				if (containerNodeIndex == 0)
-				{
-					containerNode = getContainerAttrNode(bodyNode);
-				}
-				else
-				{
-					containerNode = getContainerAttrNode(children.item(containerNodeIndex));
-				}
-				setContainerPurl(containerNode);
-			}
-			DOMWalkInformationTagger.getTextInSubTree2(bodyNode, true, bodyTextBuffy, false, true);
-		}
-		NodeList imgNodeList = doc.getElementsByTagName(HTML_TAG_IMG);
-		int numImages = imgNodeList.getLength();
-		if (numImages > 0)
-		{
-			for (int i = 0; i < numImages; i++)
-			{
-				Node imgNode = imgNodeList.item(i);
-				if (containerPurl == null)
-				{
-					setContainerPurl(getContainerAttrNode(imgNode));
-				}
-
-				// Add other information if available
-				String altText = null;
-				
-				for (int k = 0; k < imgNode.getAttributes().getLength(); k++)
-				{
-					Node att = imgNode.getAttributes().item(k);
-					// debug(att.getNodeName());
-					if (att.getNodeName().toLowerCase().equals("alt"))
-					{
-						altText = att.getNodeValue();
-					}
-				}
-
-				ImgElement im = new ImgElement(imgNode, containerPurl);
-				im.setAlt(altText);
-				imageElements.add(im);
-			}
-
-		}
-		// taggedDoc.generateCollectionsFromRoot(rootNode);
-	} 
-
-*
-*
-*/
-	
-	private static HashMap<String, Integer> namesOfBreaklineNodeNames = null;
-	
-	/**
-	 * @author rhema
-	 *  returns true when a breakline would make sense based on the node name.
+	 * This is the old parse method as of oct 13, 2011. This will be removed as soon as I am happy
+	 * tweaking the old one. -Rhema
 	 * 
-	 * @param nodeName such as p, div, br
+	 * @Override public void parse() throws IOException { org.w3c.dom.Document doc = getDom(); int
+	 * containerNodeIndex = 0; NodeList bodyNodeList = doc.getElementsByTagName(HTML_TAG_BODY);
+	 * //types not passed well... check here if (bodyNodeList.getLength() > 0) { Node bodyNode =
+	 * bodyNodeList.item(0);
+	 * 
+	 * NodeList children = bodyNode.getChildNodes(); if (children.getLength() > 0) { for (int i = 0; i
+	 * < children.getLength(); i++) { if (children.item(i).getNodeType() == Node.ELEMENT_NODE) {
+	 * containerNodeIndex = i; break; } } Node containerNode; if (containerNodeIndex == 0) {
+	 * containerNode = getContainerAttrNode(bodyNode); } else { containerNode =
+	 * getContainerAttrNode(children.item(containerNodeIndex)); } setContainerPurl(containerNode); }
+	 * DOMWalkInformationTagger.getTextInSubTree2(bodyNode, true, bodyTextBuffy, false, true); }
+	 * NodeList imgNodeList = doc.getElementsByTagName(HTML_TAG_IMG); int numImages =
+	 * imgNodeList.getLength(); if (numImages > 0) { for (int i = 0; i < numImages; i++) { Node
+	 * imgNode = imgNodeList.item(i); if (containerPurl == null) {
+	 * setContainerPurl(getContainerAttrNode(imgNode)); }
+	 * 
+	 * // Add other information if available String altText = null;
+	 * 
+	 * for (int k = 0; k < imgNode.getAttributes().getLength(); k++) { Node att =
+	 * imgNode.getAttributes().item(k); // debug(att.getNodeName()); if
+	 * (att.getNodeName().toLowerCase().equals("alt")) { altText = att.getNodeValue(); } }
+	 * 
+	 * ImgElement im = new ImgElement(imgNode, containerPurl); im.setAlt(altText);
+	 * imageElements.add(im); }
+	 * 
+	 * } // taggedDoc.generateCollectionsFromRoot(rootNode); }
+	 */
+
+	private static HashMap<String, Integer>	namesOfBreaklineNodeNames	= null;
+
+	/**
+	 * @author rhema returns true when a breakline would make sense based on the node name.
+	 * 
+	 * @param nodeName
+	 *          such as p, div, br
 	 * @return
 	 */
 	private boolean shouldBreakLineWithNodeName(String nodeName)
 	{
 		String name = nodeName.toLowerCase();
-		if(namesOfBreaklineNodeNames == null)
+		if (namesOfBreaklineNodeNames == null)
 		{
-			namesOfBreaklineNodeNames = new HashMap<String,Integer>();
-			namesOfBreaklineNodeNames.put("p",1);
-			namesOfBreaklineNodeNames.put("h1",1);
-			namesOfBreaklineNodeNames.put("h2",1);
-			namesOfBreaklineNodeNames.put("h3",1);
-			namesOfBreaklineNodeNames.put("h4",1);
-			namesOfBreaklineNodeNames.put("h5",1);
-			namesOfBreaklineNodeNames.put("h6",1);
-			namesOfBreaklineNodeNames.put("br",1);
+			namesOfBreaklineNodeNames = new HashMap<String, Integer>();
+			namesOfBreaklineNodeNames.put("p", 1);
+			namesOfBreaklineNodeNames.put("h1", 1);
+			namesOfBreaklineNodeNames.put("h2", 1);
+			namesOfBreaklineNodeNames.put("h3", 1);
+			namesOfBreaklineNodeNames.put("h4", 1);
+			namesOfBreaklineNodeNames.put("h5", 1);
+			namesOfBreaklineNodeNames.put("h6", 1);
+			namesOfBreaklineNodeNames.put("br", 1);
+			namesOfBreaklineNodeNames.put("span", 1);
 		}
 		return namesOfBreaklineNodeNames.containsKey(name);
 	}
-	
+
 	public String walkDomAddingTextAndAddNewlines(Node bodyNode)
 	{
-	  String s ="";
+		//debug("Node:" + bodyNode.getNodeName() + ":" + bodyNode.getNodeValue());
+		String s = "";
 		NodeList children = bodyNode.getChildNodes();
-		boolean addLine = false; //this is outside of the loop below to make it work correctly
-	  for (int i = 0; i < children.getLength(); i++)
-	  {	
-	  	Node kid = children.item(i);
-		//debug("Looooopy loop lop lop..."+kid.getNodeName());
-		if(addLine == false)
-		    addLine = shouldBreakLineWithNodeName(kid.getNodeName());
-		if(kid.getNodeValue() != null)
+		boolean addLine = false; // this is outside of the loop below to make it work correctly
+		for (int i = 0; i < children.getLength(); i++)
 		{
-			String v = kid.getNodeValue();
-			s = addWithOneSpaceBetween(s, v,false);
-			if(addLine)
+			Node kid = children.item(i);
+
+			if (addLine == false)
+				addLine = shouldBreakLineWithNodeName(kid.getNodeName());
+			if (kid.getNodeValue() != null)
 			{
-				s+= "\n";
-				addLine = false;
+				String v = kid.getNodeValue();
+				if (kid.getNodeName().equals("#comment"))
+					v = "";
+				s = addWithOneSpaceBetween(s, v, false);
+				if (addLine)
+				{
+					s += "\n";
+					addLine = false;
+				}
 			}
+			else if(shouldBreakLineWithNodeName(kid.getNodeName()))
+			{
+				s += "\n";
+			}
+			s = addWithOneSpaceBetween(s, walkDomAddingTextAndAddNewlines(kid), true);
 		}
-		  s = addWithOneSpaceBetween(s,walkDomAddingTextAndAddNewlines(kid),true);
-	  }
-	 
-	  return s;
+
+		//debug("Returning with:"+s);
+		return s;
 	}
-
-
 
 	private String addWithOneSpaceBetween(String s, String v, boolean newlineOK)
 	{
-		if(!newlineOK)
-		   v = v.replaceAll("\\n","");
+		if (!newlineOK)
+			v = v.replaceAll("\\n", "");
 		v = v.replaceAll("^[\\s]+", "");
-		if(v.length()>0)
+		if (v.length() > 0)
 		{
-			if(s.length()>0)
-				if(s.charAt(s.length() -1) != ' ')
+			if (s.length() > 0)
+				if (s.charAt(s.length() - 1) != ' ')
 				{
-					s+= " ";
+					s += " ";
 				}
-		   s+=v;
+			s += v;
 		}
 		return s;
 	}
-	
+
 	void checkForContainerAndSetPURL(Node bodyNode)
 	{
-		debug("looking at"+bodyNode.getNodeName());
 		bodyNode.getAttributes();
-	  if(bodyNode.getAttributes() != null)// bodyNode. .getNamedItem("container") != null)
-	  {
-	  	if(bodyNode.getAttributes().getNamedItem("container") != null)
-	  	{
-			  String containerValue = bodyNode.getAttributes().getNamedItem("container").getTextContent();
-	     if (containerValue != null && containerValue.length() > 0)
-			  {
-				  containerValue = XMLTools.unescapeXML(containerValue);
-				  containerPurl = ParsedURL.getAbsolute(containerValue);
-				//  debug("FOUND THER PURLLLL!!!"+containerValue);
-			    return;
-			  }
-	  	}
-	  }
+		if (bodyNode.getAttributes() != null)// bodyNode. .getNamedItem("container") != null)
+		{
+			if (bodyNode.getAttributes().getNamedItem("container") != null)
+			{
+				String containerValue = bodyNode.getAttributes().getNamedItem("container").getTextContent();
+				if (containerValue != null && containerValue.length() > 0)
+				{
+					containerValue = XMLTools.unescapeXML(containerValue);
+					containerPurl = ParsedURL.getAbsolute(containerValue);
+					// debug("FOUND THER PURLLLL!!!"+containerValue);
+					return;
+				}
+			}
+		}
 		NodeList children = bodyNode.getChildNodes();
-	  for (int i = 0; i < children.getLength(); i++)
-		{	
-		  checkForContainerAndSetPURL(children.item(i));
+		for (int i = 0; i < children.getLength(); i++)
+		{
+			checkForContainerAndSetPURL(children.item(i));
 		}
 	}
-	
+
 	@Override
 	public void parse() throws IOException
 	{
@@ -232,10 +190,10 @@ public class HTMLFragmentDOMParser extends HTMLDOMParser implements DOMParserInt
 		if (bodyNodeList.getLength() > 0)
 		{
 			Node bodyNode = bodyNodeList.item(0);
-			bodyTextBuffy.append( walkDomAddingTextAndAddNewlines(bodyNode));
+			bodyTextBuffy.append(walkDomAddingTextAndAddNewlines(bodyNode));
 			checkForContainerAndSetPURL(bodyNode);
 		}
-		
+
 		NodeList imgNodeList = doc.getElementsByTagName(HTML_TAG_IMG);
 		int numImages = imgNodeList.getLength();
 		if (numImages > 0)
@@ -243,7 +201,7 @@ public class HTMLFragmentDOMParser extends HTMLDOMParser implements DOMParserInt
 			for (int i = 0; i < numImages; i++)
 			{
 				Node imgNode = imgNodeList.item(i);
-				
+
 				if (containerPurl == null)
 				{
 					setContainerPurl(getContainerAttrNode(imgNode));
