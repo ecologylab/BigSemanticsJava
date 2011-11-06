d748afcee16d4dfb72f59fb33427d8ee85ce901d
rhema <rhema@ecologylab.net>
2011-11-06 22:19:17 +0000

    Added createGist().  Display only the first 8 words of a text clipping, maintaining the entire clipping's text in the context metadata.

--- ecologylab/semantics/actions/CreateAndVisualizeTextSurrogateSemanticAction.java
+++ ecologylab/semantics/actions/CreateAndVisualizeTextSurrogateSemanticAction.java
@@ -29,9 +29,28 @@ class CreateAndVisualizeTextSurrogateSemanticAction extends SemanticAction imple
 
 	}
 
+	static final int MAX_WORDS_IN_GIST = 8;
+	private String createGist(String text)
+	{
+		String[] words = text.split(" ");
+		String returnString = "";
+		int wordCount = 0;
+		for(String word : words)
+		{
+			if(wordCount > 0)
+			returnString += " ";
+			returnString += word;
+			wordCount++;
+			if(wordCount >= MAX_WORDS_IN_GIST)
+				break;
+		}
+		return returnString;
+	}
+	
 	@Override
 	public Object perform(Object obj)
 	{
+		debug("Adding text clipping");
 		boolean isSemanticText = getArgumentBoolean(SemanticActionNamedArguments.SEMANTIC_TEXT, false);
 		String context = (String) getArgumentObject(SemanticActionNamedArguments.TEXT);
 		// TODO use html context -- need methods to strip tags to set regular context from it.
@@ -39,8 +58,11 @@ class CreateAndVisualizeTextSurrogateSemanticAction extends SemanticAction imple
 		if (context != null)
 		{
 			Document sourceDocument = resolveSourceDocument();
+			//We will do something smarter here later when we have interest vectors.
 			TextClipping textClipping = new TextClipping(sessionScope.getMetaMetadataRepository().getMMByName(sessionScope.TEXT_TAG));
-			textClipping.setText(context);
+			textClipping.setText(createGist(context));
+			textClipping.setContext(context);
+
 			textClipping.setSourceDoc(sourceDocument);
 			sourceDocument.addClipping(textClipping);
 		}
