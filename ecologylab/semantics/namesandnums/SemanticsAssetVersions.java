776758a64935d0f348ea0313f54ffce745b1825d
rhema <rhema@ecologylab.net>
2011-11-16 17:08:40 +0000

    changed path for loading dictionary

--- ecologylab/semantics/namesandnums/SemanticsAssetVersions.java
+++ ecologylab/semantics/namesandnums/SemanticsAssetVersions.java
@@ -19,5 +19,5 @@ public interface SemanticsAssetVersions
 	
 	public static final float 	METAMETADATA_ASSET_VERSION		= 2.686f;
 	
-	public static final AssetsRoot	SEMANTICS_ASSETS_ROOT =  new AssetsRoot("semantics/", SingletonApplicationEnvironment.runningInEclipse() ? EnvironmentGeneric.configDir().file() : null);
+	public static final AssetsRoot	SEMANTICS_ASSETS_ROOT =  new AssetsRoot("", SingletonApplicationEnvironment.runningInEclipse() ? EnvironmentGeneric.configDir().file() : null);
 }
