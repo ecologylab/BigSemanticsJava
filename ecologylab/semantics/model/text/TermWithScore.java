ca2b96c08c4839c9244c1b363f712d30194d4622
rhema <rhema@ecologylab.net>
2011-11-16 17:10:00 +0000

    Extends Term allowing one to detach the term from a vector and change it's score arbitrarily.  This is useful in the context of normalizing terms.

---
+++ ecologylab/semantics/model/text/TermWithScore.java
@@ -0,0 +1,53 @@
+package ecologylab.semantics.model.text;
+/***
+ * 
+ * @author rhema
+ *  Extends Term allowing one to detach the term from a vector
+ *  and change it's score arbitrarily.  This is useful
+ *  in the context of normalizing terms.
+ */
+
+public class TermWithScore extends Term
+{
+	private double score;
+	public double getScore()
+	{
+		return score;
+	}
+
+	public void setScore(double score)
+	{
+		this.score = score;
+	}
+
+	public TermWithScore()
+	{
+		super();
+	}
+	
+	public TermWithScore(Term t, double score)
+	{
+		super();
+		this.setWord(t.getWord());
+		this.score = score;
+	}
+	
+	public String toString()
+	{
+		return this.getWord() + ":" + score;
+	}
+	
+	@Override
+	public int compareTo(Term o) 
+	{
+		TermWithScore convertedTerm = (TermWithScore) o;
+		double difference = this.getScore() - convertedTerm.getScore();
+			
+		if (difference > 0)
+			return -1;
+		else
+			return (difference == 0) ? this.getWord().compareTo(convertedTerm.getWord()) : 1;
+	}
+
+	
+}
