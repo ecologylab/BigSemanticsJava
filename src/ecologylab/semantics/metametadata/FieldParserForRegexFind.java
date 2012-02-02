7d640d3954e9d93eada579885ec49ed640f5a03d
quyin <quyin@ecologylab.net>
2012-02-02 00:43:30 +0000

    small enhancement to field_parser_for_regex_find.

--- src/ecologylab/semantics/metametadata/FieldParserForRegexFind.java
+++ src/ecologylab/semantics/metametadata/FieldParserForRegexFind.java
@@ -15,16 +15,19 @@ public class FieldParserForRegexFind extends FieldParser
 	public Map<String, String> getKeyValuePairResult(FieldParserElement parserElement, String input)
 	{
 		Map<String, String> rst = new HashMap<String, String>();
-
-		Pattern p = parserElement.getRegex();
-		Matcher m = p.matcher(input);
-		int j=0;
-		while (m.find())
+		
+		if (input != null)
 		{
-			for (int i=0; i <= m.groupCount(); i++)
+			Pattern p = parserElement.getRegex();
+			Matcher m = p.matcher(input);
+			int j=0;
+			while (m.find())
 			{
-				String value = m.group(i);
-				rst.put(groupPrefix + j++, value);
+				for (int i=0; i <= m.groupCount(); i++)
+				{
+					String value = m.group(i);
+					rst.put(groupPrefix + j++, value);
+				}
 			}
 		}
 
@@ -34,8 +37,8 @@ public class FieldParserForRegexFind extends FieldParser
 	@Test
 	public void testFind()
 	{
-		String test = "2007. Mixed media, sound, pneumatics, robotics, elector magnetic beaters, dentist chair, electric guitar, computer, various control systems, 9' 10\" x 13' 1\" x 8' 2\" (118 x 157 x 98 cm) 5 min. Gift of the Julia Stoschek Foundation, Düsseldorf, and the Dunn Bequest. © 2011 Janet Cardiff and George Bures Miller. Photo: Ugarte & Lorena Lopez. Courtesy of the artist, Luhring Augustine, New York and Galerie Barbara Weiss, Berlin. ";
-		//String test = "(2007). Ink on paper, 13 7/8 x 9 7/8\" (35.2 x 25.1 cm). Purchased with funds provided by Jill and Peter Kraus. © 2011 Bernd Ribbeck";
+		String test = "2007. Mixed media, sound, pneumatics, robotics, elector magnetic beaters, dentist chair, electric guitar, computer, various control systems, 9' 10\" x 13' 1\" x 8' 2\" (118 x 157 x 98 cm) 5 min. Gift of the Julia Stoschek Foundation, Dï¿½sseldorf, and the Dunn Bequest. ï¿½ 2011 Janet Cardiff and George Bures Miller. Photo: Ugarte & Lorena Lopez. Courtesy of the artist, Luhring Augustine, New York and Galerie Barbara Weiss, Berlin. ";
+		//String test = "(2007). Ink on paper, 13 7/8 x 9 7/8\" (35.2 x 25.1 cm). Purchased with funds provided by Jill and Peter Kraus. ï¿½ 2011 Bernd Ribbeck";
 		FieldParserElement pe = new FieldParserElement("regex_find", Pattern.compile("(\\S.+?)\\. ")
 				/*"([^. ]\\S.+?)(?=[.])"*/);
 		Map<String, String> rst = getKeyValuePairResult(pe, test);
