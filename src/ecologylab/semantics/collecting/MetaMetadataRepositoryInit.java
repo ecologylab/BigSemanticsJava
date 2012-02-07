78f51906fd166597dbd26196d0d9b19a3ca4172d
shenfeng <shenfeng@ecologylab.net>
2012-02-07 06:25:04 +0000

    More work on separating the ecologylabFundamental and ecologylabSemantics for Android done. ecologylabFundamentalSun part is working. ecologylabFundamentalAndroid part is not (because of the SemanticSite.java in ecologylab.Semantics.collection). will fix it tomorrow.

--- src/ecologylab/semantics/collecting/MetaMetadataRepositoryInit.java
+++ src/ecologylab/semantics/collecting/MetaMetadataRepositoryInit.java
@@ -4,6 +4,8 @@
 package ecologylab.semantics.collecting;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 
 import ecologylab.appframework.ApplicationProperties;
@@ -31,29 +33,29 @@ import ecologylab.serialization.SimplTypesScope.GRAPH_SWITCH;
 import ecologylab.serialization.formatenums.Format;
 
 /**
- * Initializes the MetaMetadataRepository, using its standard location in the /repository directory of the ecologylabSemantics project,
- * or its associated zip file in the Assets cache.
+ * Initializes the MetaMetadataRepository, using its standard location in the /repository directory
+ * of the ecologylabSemantics project, or its associated zip file in the Assets cache.
  * 
  * @author andruid
  */
-public class MetaMetadataRepositoryInit extends Scope<Object>
-implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
+public class MetaMetadataRepositoryInit extends Scope<Object> implements DocumentParserTagNames,
+		ApplicationProperties, SemanticsNames
 {
-	
+
 	public static Format												DEFAULT_REPOSITORY_FORMAT		= Format.XML;
 
 	public static MetaMetadataRepositoryLoader	DEFAULT_REPOSITORY_LOADER		= new MetaMetadataRepositoryLoader();
 
 	private static final String									SEMANTICS										= "semantics/";
-	
+
 	public static final String									DEFAULT_REPOSITORY_LOCATION	= "../../MetaMetadataRepository/MmdRepository/mmdrepository";
 
 	static
 	{
-		SimplTypesScope.graphSwitch	= GRAPH_SWITCH.ON;
+		SimplTypesScope.graphSwitch = GRAPH_SWITCH.ON;
 		MetaMetadataRepository.initializeTypes();
 	}
-	
+
 	private File																repositoryLocation;
 
 	private MetaMetadataRepository							metaMetadataRepository;
@@ -67,35 +69,45 @@ implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
 	private final SimplTypesScope								repositoryClippingTranslations;
 
 	public MetaMetadata													DOCUMENT_META_METADATA;
+
 	public MetaMetadata													PDF_META_METADATA;
+
 	public MetaMetadata													SEARCH_META_METADATA;
+
 	public MetaMetadata													IMAGE_META_METADATA;
+
 	public MetaMetadata													DEBUG_META_METADATA;
+
 	public MetaMetadata													IMAGE_CLIPPING_META_METADATA;
 
 	protected MetaMetadataRepositoryInit(SimplTypesScope metadataTranslationScope)
 	{
 		this(null, metadataTranslationScope);
 	}
-	
-	protected MetaMetadataRepositoryInit(File repositoryLocation, SimplTypesScope metadataTranslationScope)
+
+	protected MetaMetadataRepositoryInit(File repositoryLocation,
+			SimplTypesScope metadataTranslationScope)
 	{
 		this(repositoryLocation, DEFAULT_REPOSITORY_FORMAT, metadataTranslationScope);
 	}
-	
+
 	/**
 	 * This constructor should only be called from SemanticsScope's constructor!
 	 * 
 	 * @param metadataTranslationScope
 	 */
-	protected MetaMetadataRepositoryInit(File repositoryLocation, Format repositoryFormat, SimplTypesScope metadataTranslationScope)
+	protected MetaMetadataRepositoryInit(File repositoryLocation, Format repositoryFormat,
+			SimplTypesScope metadataTranslationScope)
 	{
-		if (SingletonApplicationEnvironment.isInUse() && !SingletonApplicationEnvironment.runningInEclipse())
+		if (SingletonApplicationEnvironment.isInUse()
+				&& !SingletonApplicationEnvironment.runningInEclipse())
 		{
 			ParsedURL semanticsRelativeUrl = EnvironmentGeneric.configDir().getRelative(SEMANTICS);
-			File repositoryDir = Files.newFile(PropertiesAndDirectories.thisApplicationDir(), SEMANTICS + "/repository");
+			File repositoryDir = Files.newFile(PropertiesAndDirectories.thisApplicationDir(), SEMANTICS
+					+ "/repository");
 			AssetsRoot mmAssetsRoot = new AssetsRoot(semanticsRelativeUrl, repositoryDir);
-			repositoryLocation 	= Assets.getAsset(mmAssetsRoot, null, "repository", null, !USE_ASSETS_CACHE, SemanticsAssetVersions.METAMETADATA_ASSET_VERSION);
+			repositoryLocation = Assets.getAsset(mmAssetsRoot, null, "repository", null,
+					!USE_ASSETS_CACHE, SemanticsAssetVersions.METAMETADATA_ASSET_VERSION);
 			Debug.debugT(this, "tentative meta-metadata repository location: " + repositoryLocation);
 		}
 		else
@@ -104,26 +116,30 @@ implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
 				repositoryLocation = findRepositoryLocation();
 		}
 		this.repositoryLocation = repositoryLocation;
-		
+
 		Debug.println("\t\t-- Reading meta_metadata from " + repositoryLocation);
-		metaMetadataRepository 					= DEFAULT_REPOSITORY_LOADER.loadFromDir(repositoryLocation, repositoryFormat);
+		metaMetadataRepository = DEFAULT_REPOSITORY_LOADER.loadFromDir(repositoryLocation,
+				repositoryFormat);
 		if (metaMetadataRepository != null)
 		{
-			DOCUMENT_META_METADATA						= metaMetadataRepository.getMMByName(DOCUMENT_TAG);
-			PDF_META_METADATA									= metaMetadataRepository.getMMByName(PDF_TAG);
-			SEARCH_META_METADATA							= metaMetadataRepository.getMMByName(SEARCH_TAG);
-			IMAGE_META_METADATA								= metaMetadataRepository.getMMByName(IMAGE_TAG);
-			DEBUG_META_METADATA								= metaMetadataRepository.getMMByName(DEBUG_TAG);
-			IMAGE_CLIPPING_META_METADATA			= metaMetadataRepository.getMMByName(IMAGE_CLIPPING_TAG);
-			
-			this.metadataTranslationScope	= metadataTranslationScope;
-			this.generatedDocumentTranslations	= metadataTranslationScope.getAssignableSubset(REPOSITORY_DOCUMENT_TRANSLATIONS, Document.class);
-			this.generatedMediaTranslations	= metadataTranslationScope.getAssignableSubset(REPOSITORY_MEDIA_TRANSLATIONS, ClippableDocument.class);
-			this.repositoryClippingTranslations = metadataTranslationScope.getAssignableSubset(REPOSITORY_CLIPPING_TRANSLATIONS, Clipping.class);
-			
+			DOCUMENT_META_METADATA = metaMetadataRepository.getMMByName(DOCUMENT_TAG);
+			PDF_META_METADATA = metaMetadataRepository.getMMByName(PDF_TAG);
+			SEARCH_META_METADATA = metaMetadataRepository.getMMByName(SEARCH_TAG);
+			IMAGE_META_METADATA = metaMetadataRepository.getMMByName(IMAGE_TAG);
+			DEBUG_META_METADATA = metaMetadataRepository.getMMByName(DEBUG_TAG);
+			IMAGE_CLIPPING_META_METADATA = metaMetadataRepository.getMMByName(IMAGE_CLIPPING_TAG);
+
+			this.metadataTranslationScope = metadataTranslationScope;
+			this.generatedDocumentTranslations = metadataTranslationScope.getAssignableSubset(
+					REPOSITORY_DOCUMENT_TRANSLATIONS, Document.class);
+			this.generatedMediaTranslations = metadataTranslationScope.getAssignableSubset(
+					REPOSITORY_MEDIA_TRANSLATIONS, ClippableDocument.class);
+			this.repositoryClippingTranslations = metadataTranslationScope.getAssignableSubset(
+					REPOSITORY_CLIPPING_TRANSLATIONS, Clipping.class);
+
 			this.generatedMediaTranslations.addTranslation(Clipping.class);
 			this.generatedMediaTranslations.addTranslation(Annotation.class);
-			
+
 			metaMetadataRepository.bindMetadataClassDescriptorsToMetaMetadata(metadataTranslationScope);
 		}
 		else
@@ -131,17 +147,28 @@ implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
 			throw new RuntimeException("FAILURE TO LOAD META-METADATA REPOSITORY!!!");
 		}
 	}
-	
+
 	/**
-	 * Find meta-metadata repository location. Look up the location by using Java resources. If failed, use the default location
-	 * as specified by the crossPlatformSemantics project.
+	 * Find meta-metadata repository location. Look up the location by using Java resources. If
+	 * failed, use the default location as specified by the crossPlatformSemantics project.
 	 * 
 	 * @return The repository location.
 	 */
 	public static File findRepositoryLocation()
 	{
 		File repositoryLocation = null;
-		
+
+		if (PropertiesAndDirectories.os() == PropertiesAndDirectories.ANDROID)
+			repositoryLocation = findRepositoryLocationForAndroid();
+		else
+			repositoryLocation = findRepositoryLocationForSun();
+
+		return repositoryLocation;
+	}
+
+	private static File findRepositoryLocationForSun()
+	{
+		File repositoryLocation = null;
 		URL repositoryUrl = MetaMetadataRepositoryInit.class.getResource("/mmdrepository");
 		String repositoryPath = repositoryUrl.getFile();
 		if (repositoryPath != null)
@@ -150,25 +177,82 @@ implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
 			if (!repositoryLocation.exists())
 			{
 				// repository not found by resource
-				Debug.warning(MetaMetadataRepositoryInit.class, "Cannot locate meta-metadata repository by resource: is MetaMetadataRepository project in your development environment?");
+				Debug
+						.warning(
+								MetaMetadataRepositoryInit.class,
+								"Cannot locate meta-metadata repository by resource: is MetaMetadataRepository project in your development environment?");
 				Debug.warning(MetaMetadataRepositoryInit.class, "Use default repository path.");
 				repositoryLocation = new File(DEFAULT_REPOSITORY_LOCATION);
 				if (!repositoryLocation.exists())
 				{
-					Debug.warning(MetaMetadataRepositoryInit.class, "Cannot locate meta-metadata repository by default: please upgrade to the new corss-platform semantics layout!!");
-					Debug.warning(MetaMetadataRepositoryInit.class, "Fallback to the old default location -- THIS WILL NOT BE SUPPORTED SOON!!");
+					Debug
+							.warning(
+									MetaMetadataRepositoryInit.class,
+									"Cannot locate meta-metadata repository by default: please upgrade to the new corss-platform semantics layout!!");
+					Debug.warning(MetaMetadataRepositoryInit.class,
+							"Fallback to the old default location -- THIS WILL NOT BE SUPPORTED SOON!!");
 					repositoryLocation = new File("../ecologylabSemantics/repository");
 					if (!repositoryLocation.exists())
 					{
-						Debug.error(MetaMetadataRepositoryInit.class, "Failure to locate meta-metadata repository!");
+						Debug.error(MetaMetadataRepositoryInit.class,
+								"Failure to locate meta-metadata repository!");
 					}
 				}
 			}
 		}
-		
 		return repositoryLocation;
 	}
-	
+
+	private static File findRepositoryLocationForAndroid()
+	{
+		Class environmentClass;
+		try
+		{
+			environmentClass = Class.forName("android.os.Environment");
+			if (environmentClass != null)
+			{
+				Method m = environmentClass.getMethod("getExternalStorageDirectory");
+				File sdCard = (File) m.invoke(null, null);
+				File ecologylabDir = new File(sdCard.getAbsolutePath()
+						+ "/Android/data/com.ecologyAndroid.ecoDroidTest/files/");
+				File mmdrepositoryDir = new File(ecologylabDir + "/mmdrepository/");
+				return mmdrepositoryDir;
+			}
+		}
+		catch (ClassNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (SecurityException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (NoSuchMethodException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (InvocationTargetException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
 	/**
 	 * @return The repository location.
 	 */
@@ -201,7 +285,6 @@ implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
 		return generatedDocumentTranslations;
 	}
 
-
 	/**
 	 * @return the generatedMediaTranslations.
 	 */
@@ -209,7 +292,7 @@ implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
 	{
 		return generatedMediaTranslations;
 	}
-	
+
 	/**
 	 * @return the repositoryClippingTranslations.
 	 */
@@ -217,5 +300,5 @@ implements DocumentParserTagNames, ApplicationProperties, SemanticsNames
 	{
 		return repositoryClippingTranslations;
 	}
-	
+
 }
