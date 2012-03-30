a223aac0212439d25414242098a60a7bd256f088
quyin <quyin@ecologylab.net>
2012-03-30 02:56:16 +0000

    prevent null pointer exception in IfActionException.

--- src/ecologylab/semantics/actions/exceptions/IfActionException.java
+++ src/ecologylab/semantics/actions/exceptions/IfActionException.java
@@ -15,9 +15,12 @@ public class IfActionException extends SemanticActionExecutionException
 		super(action);
 		System.out.println(":::All the nested semantic actions might not execute properly:::");
 		ArrayList<SemanticAction> nestedActions= action.getNestedSemanticActionList();
-		for(int i=0;i<nestedActions.size();i++)
+		if (nestedActions != null && nestedActions.size() > 0)
 		{
-			System.out.println("\t\t\t["+nestedActions.get(i).getActionName().toUpperCase()+"] skipped");
+			for(int i=0;i<nestedActions.size();i++)
+			{
+				System.out.println("\t\t\t["+nestedActions.get(i).getActionName().toUpperCase()+"] skipped");
+			}
 		}
 		stackTrace(semanticActionReturnValueMap);
 	}
