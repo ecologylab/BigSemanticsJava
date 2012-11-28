5dce42782c8fb2d5de3630cd5dc77c8b55d346b2
Yin Qu <yin@ecologylab.net>
2012-11-28 15:37:00 -0600

    fixed bug in copying additional locations for cached documents.

--- src/ecologylab/semantics/metadata/builtins/Document.java
+++ src/ecologylab/semantics/metadata/builtins/Document.java
@@ -503,13 +503,11 @@ public class Document extends DocumentDeclaration
 			for (Metadata oldMixin : oldMixins)
 				addMixin(oldMixin);
 
-		if (getAdditionalLocations() != null)
+		List<MetadataParsedURL> oldAdditionalLocations = oldDocument.getAdditionalLocations();
+		if (oldAdditionalLocations != null)
 		{
-  		List<MetadataParsedURL> oldAdditionalLocations =
-  		    new ArrayList<MetadataParsedURL>(getAdditionalLocations());
-  		if (oldAdditionalLocations != null)
-  			for (MetadataParsedURL otherLocation : oldAdditionalLocations)
-  				addAdditionalLocation(otherLocation);
+			for (MetadataParsedURL otherLocation : oldAdditionalLocations)
+				addAdditionalLocation(otherLocation);
 		}
 		
 		//TODO -- are there other values that should be propagated?! -- can use MetadataFieldDescriptors.
