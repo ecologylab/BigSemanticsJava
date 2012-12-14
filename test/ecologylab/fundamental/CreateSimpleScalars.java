32a39898c24b62994c4ed39dc1e42ed8f7844468
Tom White <tom.white@outlook.com>
2012-12-14 16:56:43 -0600

    Adding a few more tests

--- test/ecologylab/fundamental/CreateSimpleScalars.java
+++ test/ecologylab/fundamental/CreateSimpleScalars.java
@@ -2,6 +2,8 @@ package ecologylab.fundamental;
 
 import java.io.File;
 import java.io.IOException;
+import com.google.common.io.Files;
+import com.google.common.base.Charsets;
 
 public class CreateSimpleScalars {
 
@@ -44,7 +46,8 @@ public class CreateSimpleScalars {
 	static void Create(String name, String type, String include) throws IOException
 	{
 		File f = getFor(name);
-		//com.google.common.io.Files.write(getRepr(name, type, include), f, Charsets.UTF_8);
+
+		Files.write(getRepr(name, type, include), f, Charsets.UTF_8);
 	}
 	
 	/**
