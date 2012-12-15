3f469300ca65be0014e1514ee6a63c9cb82ef02b
Tom White <tom.white@outlook.com>
2012-12-14 18:10:54 -0600

    What is this, I don't even? One passes, the other doesnt.

--- test/ecologylab/semantics/metametadata/TestBuiltinDescription.java
+++ test/ecologylab/semantics/metametadata/TestBuiltinDescription.java
@@ -1,18 +1,39 @@
 package ecologylab.semantics.metametadata;
 
 import static org.junit.Assert.*;
+import ecologylab.semantics.generated.library.RepositoryMetadataTranslationScope;
 import ecologylab.semantics.metadata.MetadataClassDescriptor;
 import ecologylab.semantics.metadata.builtins.declarations.MetadataDeclaration;
+import ecologylab.semantics.metadata.scalar.MetadataString;
 import ecologylab.serialization.ClassDescriptor;
+import ecologylab.serialization.SimplTypesScope;
 
 
 import org.junit.Test;
 
 public class TestBuiltinDescription {
 
+	
 	@Test
-	public void test() {
-		ClassDescriptor<?> cd = ClassDescriptor.getClassDescriptor(MetadataDeclaration.class);		
+	public void testThatTheRepositoryScopeExists()
+	{
+		
+		SimplTypesScope defaultGet = RepositoryMetadataTranslationScope.get();
+		
+		assertNotNull(defaultGet);
+		
+		SimplTypesScope sts = SimplTypesScope.get("repository_metadata");
+		
+		assertNotNull(sts);		
+		
+	}
+	@Test
+	public void testThatCreationDoesntThrowAnException() {
+		SimplTypesScope defaultGet = RepositoryMetadataTranslationScope.get();
+
+		ClassDescriptor<?> cd = ClassDescriptor.getClassDescriptor(MetadataString.class);	
+		ClassDescriptor<?> mdCD = ClassDescriptor.getClassDescriptor(MetadataDeclaration.class);
+		MetadataDeclaration md = new MetadataDeclaration();
 	}
 
 }
