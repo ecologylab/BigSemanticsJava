cf9d1abca13b6e4194b8d282ac8832dbcf0ad8ba
Tom White <tom.white@outlook.com>
2013-01-04 16:29:31 -0700

    adding extra tests

--- test/ecologylab/fundamental/DBALTests.java
+++ test/ecologylab/fundamental/DBALTests.java
@@ -9,11 +9,14 @@ import ecologylab.serialization.ClassDescriptor;
 import ecologylab.serialization.FieldDescriptor;
 import ecologylab.serialization.annotations.FieldUsage;
 import ecologylab.serialization.annotations.simpl_collection;
+import ecologylab.serialization.annotations.simpl_scalar;
 
 public class DBALTests {
+	//TODO: validation on these is super sparse... just to prove out where
+	// exceptions happen. Will fix eventually. 
 
 	@Test
-	public void simpl_Collection_transmitsNameIntoFD()
+	public void simpl_Collection_of_enums_transmitsNameIntoFD()
 	{
 		final class myCollectionClass
 		{
@@ -25,4 +28,54 @@ public class DBALTests {
 		FieldDescriptor fd = (FieldDescriptor)cd.allFieldDescriptors().get(0);
 		assertNotNull(fd.getTagName());
 	}
+	
+	@Test
+	public void simpl_collection_of_composites_transmistNameIntoFD()
+	{
+		final class someCompositeClass
+		{
+			@simpl_scalar
+			private Integer myInteger;
+		}
+		
+		final class myCollectionClass
+		{
+			@simpl_collection("excluded_usage")
+			private ArrayList<someCompositeClass> excludedUsages;	
+		}
+		
+		ClassDescriptor cd = ClassDescriptor.getClassDescriptor(myCollectionClass.class);
+		FieldDescriptor fd = (FieldDescriptor)cd.allFieldDescriptors().get(0);
+		assertNotNull(fd.getTagName());
+	}
+	
+	@Test
+	public void simpl_Collection_of_scalars_transmitsNameIntoFD()
+	{
+		final class myCollectionClass
+		{
+			@simpl_collection("excluded_usage")
+			private ArrayList<Integer> excludedUsages;	
+		}
+		
+		ClassDescriptor cd = ClassDescriptor.getClassDescriptor(myCollectionClass.class);
+		FieldDescriptor fd = (FieldDescriptor)cd.allFieldDescriptors().get(0);
+		assertNotNull(fd.getTagName());
+	}
+	
+	@Test
+	public void simpl_collection_of_collection_of_scalars_works()
+	{
+		final class myCollectionClass
+		{
+			@simpl_collection("excluded_usage")
+			private ArrayList<ArrayList<Integer>> excludedUsages;	
+		}
+		
+		ClassDescriptor cd = ClassDescriptor.getClassDescriptor(myCollectionClass.class);
+		FieldDescriptor fd = (FieldDescriptor)cd.allFieldDescriptors().get(0);
+		assertNotNull(fd.getTagName());	
+	}
+	
+	
 }
