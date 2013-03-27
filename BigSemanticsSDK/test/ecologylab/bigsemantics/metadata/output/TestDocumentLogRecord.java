e64ea5ff5a8143be9ff5ecd82959a2c02b924009
ajitjain <ajit.infotech@gmail.com>
2013-03-27 14:00:33 -0500

    more logrecord assertions; use isEmpty

--- BigSemanticsSDK/test/ecologylab/bigsemantics/metadata/output/TestDocumentLogRecord.java
+++ BigSemanticsSDK/test/ecologylab/bigsemantics/metadata/output/TestDocumentLogRecord.java
@@ -17,62 +17,78 @@ import ecologylab.serialization.SIMPLTranslationException;
 import ecologylab.serialization.SimplTypesScope;
 import ecologylab.serialization.formatenums.StringFormat;
 
-public class TestDocumentLogRecord {
-	
-	private Object lockDoc = new Object();
-	
+public class TestDocumentLogRecord
+{
+
+	private Object	lockDoc	= new Object();
+
 	@Test
 	public void testTimeIntervalValues() throws InterruptedException
 	{
-	    DocumentLogRecord logRecord = new DocumentLogRecord();
-	    
-	    SimplTypesScope metadataTScope = RepositoryMetadataTranslationScope.get();
-		SemanticsSessionScope sss = new SemanticsSessionScope(metadataTScope,
-				                                              CybernekoWrapper.class);
+		DocumentLogRecord logRecord = new DocumentLogRecord();
+
+		SimplTypesScope metadataTScope = RepositoryMetadataTranslationScope.get();
+		SemanticsSessionScope sss = new SemanticsSessionScope(metadataTScope, CybernekoWrapper.class);
 		ParsedURL purl = ParsedURL.getAbsolute("http://dl.acm.org/citation.cfm?id=1835572");
-	    Document doc = sss.getOrConstructDocument(purl);
-	    DocumentClosure closure = doc.getOrConstructClosure();
-	    closure.setLogRecord(logRecord);
-	    closure.addContinuation(new Continuation<DocumentClosure>() {
+		Document doc = sss.getOrConstructDocument(purl);
+		DocumentClosure closure = doc.getOrConstructClosure();
+		closure.setLogRecord(logRecord);
+		closure.addContinuation(new Continuation<DocumentClosure>()
+		{
 			@Override
-			public void callback(DocumentClosure closure) {
-			    synchronized(lockDoc)
-			    {
-			        lockDoc.notify();
-			    }
+			public void callback(DocumentClosure closure)
+			{
+				synchronized (lockDoc)
+				{
+					lockDoc.notify();
+				}
 			}
 		});
-	    closure.queueDownload();
-	    synchronized (lockDoc)
-	    {
-	    	lockDoc.wait();
-	    }
-	    
-	    Assert.assertNotNull(logRecord.getQueuePeekIntervals());
-	    Assert.assertTrue(logRecord.getQueuePeekIntervals().size() > 0);
-	    
-	    Assert.assertTrue(logRecord.getmSecInExtraction() > 0);
-	    Assert.assertTrue(logRecord.getmSecInHtmlDownload() > 0);
-	    Assert.assertTrue(logRecord.getEnQueueTimestamp() > 0);
+		closure.queueDownload();
+		synchronized (lockDoc)
+		{
+			lockDoc.wait();
+		}
+
+		Assert.assertNotNull(logRecord.getQueuePeekIntervals());
+		Assert.assertFalse(logRecord.getQueuePeekIntervals().isEmpty());
+
+		Assert.assertTrue(logRecord.getmSecInExtraction() > 0);
+		Assert.assertTrue(logRecord.getmSecInHtmlDownload() > 0);
+		Assert.assertTrue(logRecord.getEnQueueTimestamp() > 0);
 	}
-	
+
+	private String serializeToJson(Object myObject) throws SIMPLTranslationException
+	{
+		return SimplTypesScope.serialize(myObject, StringFormat.JSON).toString();
+	}
+
 	@Test
-  public void testDeserializeDownloadableLogRecord() throws SIMPLTranslationException
-  {
-  	DownloadableLogRecord logRecord = new DownloadableLogRecord();
-  	ArrayList<Long> peekIntervals = new ArrayList<Long>();
-  	peekIntervals.add(100L);
-  	peekIntervals.add(1000L);
+	public void testDeserializeDownloadableLogRecord() throws SIMPLTranslationException
+	{
+		DownloadableLogRecord logRecord = new DownloadableLogRecord();
+		
+		logRecord.setHtmlCacheHit(false);
+		logRecord.setUrlHash("eqwewqewqe");
+		ArrayList<Long> peekIntervals = new ArrayList<Long>();
+		peekIntervals.add(100L);
+		peekIntervals.add(1000L);
 		logRecord.setQueuePeekIntervals(peekIntervals);
-		
-		String json = SimplTypesScope.serialize(logRecord, StringFormat.JSON).toString();
-		System.out.println(json);
-		
+
+		String json = serializeToJson(logRecord);
+		Assert.assertNotNull(json);
+		Assert.assertTrue(json.length() > 0);
+
 		SimplTypesScope tscope = SimplTypesScope.get("test-deserializing-downloadable-log-record",
 				DownloadableLogRecord.class);
 		logRecord = (DownloadableLogRecord) tscope.deserialize(json, StringFormat.JSON);
+		
 		Assert.assertNotNull(logRecord.getQueuePeekIntervals());
-    Assert.assertTrue(logRecord.getQueuePeekIntervals().size() > 0);
-  }
+		Assert.assertFalse(logRecord.getQueuePeekIntervals().isEmpty());
+		Assert.assertFalse(logRecord.isHtmlCacheHit());
+		Assert.assertTrue(logRecord.getUrlHash().equals("eqwewqewqe"));
+		Assert.assertTrue(logRecord.getQueuePeekIntervals().get(0) == 100L);
+		Assert.assertTrue(logRecord.getQueuePeekIntervals().get(1) == 1000L);
+	}
 
 }
