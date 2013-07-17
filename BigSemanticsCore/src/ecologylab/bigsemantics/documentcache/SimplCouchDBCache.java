f3ac43ba9f4910fa929bc7529fbcde4f93f6621d
Colton Riedel <crriedel@tlu.edu>
2013-07-17 17:36:48 -0500

    Added factory method to SimplCouchDBCache, commit of Apache HTTP jars

--- BigSemanticsCore/src/ecologylab/bigsemantics/documentcache/SimplCouchDBCache.java
+++ BigSemanticsCore/src/ecologylab/bigsemantics/documentcache/SimplCouchDBCache.java
@@ -5,12 +5,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
-import org.apache.http.client.*;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.*;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.ClientProtocolException;
 
 import ecologylab.serialization.SimplTypesScope;
 import ecologylab.serialization.formatenums.StringFormat;
@@ -24,14 +27,23 @@ public class SimplCouchDBCache implements ISimplCache
 {
 	private DefaultHttpClient client;
 
-	private SemanticsSessionScope sss;
+	private SemanticsSessionScope sss = null;
 	private SimplTypesScope metadataTypesScope;
 
 	private static String host = "localhost";
 	private static String port = "5984";
-	private static String database = "testdb3";
+	private static String database = "exampledb";
 
-	public SimplCouchDBCache()
+	public static SimplCouchDBCache simplCouchDBCacheFactory(SemanticsSessionScope sss)
+	{
+		SimplCouchDBCache cache = new SimplCouchDBCache();
+		
+		cache.setSemanticsSessionScope(sss);
+		
+		return cache;
+	}
+	
+	private SimplCouchDBCache()
 	{
 		client = new DefaultHttpClient();
 	}
