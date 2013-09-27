7592c3acf907c8d4600edebfd9b33f7455eda98d
amwebb <andrew@ecologylab.net>
2013-09-27 10:25:56 -0500

    add new rich artifacts type scope

---
+++ BigSemanticsCore/src/ecologylab/bigsemantics/metadata/builtins/RichArtifactsTypesScope.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package ecologylab.bigsemantics.metadata.builtins;
+
+import ecologylab.bigsemantics.metadata.Metadata;
+import ecologylab.bigsemantics.metadata.builtins.declarations.MetadataBuiltinDeclarationsTranslationScope;
+import ecologylab.bigsemantics.metadata.scalar.types.MetadataScalarType;
+import ecologylab.generic.Debug;
+import ecologylab.serialization.SimplTypesScope;
+
+/**
+ * Encapsulates ClassDescriptors for Metadata and its subclasses that are coded by hand.
+ */
+public class RichArtifactsTypesScope extends Debug
+{
+	public static final String NAME = "rich_artifacts_scope";
+	
+	protected static final Class CLASSES[] = 
+	{
+		RichArtifact.class,
+		RichBookmark.class,
+		CreativeAct.class,
+		ImageSelfmade.class,
+		TextSelfmade.class,
+		Clipping.class,
+		ImageClipping.class,
+		TextClipping.class,
+		CurateLink.class,
+		Annotate.class,
+		
+	};
+	
+	static
+	{
+		MetadataScalarType.init();
+	}
+
+	public static SimplTypesScope get()
+	{
+		return SimplTypesScope.get(NAME, MetadataBuiltinDeclarationsTranslationScope.get(), MetadataMediaTypesScope.get(), CLASSES);
+	}	
+}
